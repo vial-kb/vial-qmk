c77aa8db6f220e1842de28fbc2b42341bc87a30e
juank <juank@fktech.net>
2021-04-19 23:07:43 -0300

    Solve missing quantum.h on WS2812 driver for AVR

--- drivers/avr/ws2812.c
+++ drivers/avr/ws2812.c
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "quantum.h"
 #include "ws2812.h"
 #include <avr/interrupt.h>
 #include <avr/io.h>
