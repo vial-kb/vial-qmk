b48a0b3bde7b19db48695871808501d58429481c
trashcatt <chance@trashcat.xyz>
2021-06-15 19:08:20 -0400

    added unlock combo

--- keyboards/bpiphany/frosty_flake/config.h
+++ keyboards/bpiphany/frosty_flake/config.h
@@ -139,4 +139,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+// Unlock Combo
+#define VIAL_UNLOCK_COMBO_ROWS { 9, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 6, 1 }
 #endif
