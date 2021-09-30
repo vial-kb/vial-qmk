336c5a9d750468dce0a99109b064ab4e555634f3
Ilya Zhuravlev <whatever@xyz.is>
2021-09-30 13:16:41 -0400

    Add dynamic key overrides support

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -555,6 +555,7 @@ ifeq ($(strip $(VIAL_ENABLE)), yes)
     QMK_SETTINGS ?= yes
     TAP_DANCE_ENABLE ?= yes
     COMBO_ENABLE ?= yes
+    KEY_OVERRIDE_ENABLE ?= yes
     SRC += $(QUANTUM_DIR)/vial.c
     EXTRAINCDIRS += $(KEYMAP_OUTPUT)
     OPT_DEFS += -DVIAL_ENABLE -DNO_DEBUG -DSERIAL_NUMBER=\"vial:f64c2b3c\"
