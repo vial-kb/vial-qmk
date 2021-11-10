bcc40f10a5024db69ce35ec1d767b127e7eb1e6e
David Gillies <dave.gillies@gmail.com>
2021-11-09 22:11:20 -0800

    Add vial support for RoMac

---
+++ keyboards/kingly_keys/romac/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4F, 0x9A, 0x90, 0xD0, 0x29, 0xF8, 0x8E, 0x35}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
