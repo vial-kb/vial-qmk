c0dcb5276d714f1b7df80c093a92229d0581a544
ImprezaXZ <imprezaxluv@gmail.com>
2021-11-18 17:06:03 +0200

    vial support for dumbpad, v1x, left, single encoder

--- keyboards/dumbpad/config.h
+++ keyboards/dumbpad/config.h
@@ -32,3 +32,40 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Reduce tapdance required taps from 5 to 2 */
 #define TAPPING_TOGGLE 2
+
+/* USB Device descriptor parameter */
+#define DEVICE_VER      0x0010
+
+/* Column/Row IO definitions */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 5
+#define MATRIX_ROW_PINS { F4, F5, F6, F7 }
+#define MATRIX_COL_PINS { C6, D7, E6, B4, B5 }
+#define UNUSED_PINS
+
+
+
+/* Rotary encoder */
+#define ENCODERS_PAD_A { B2, D0 }
+#define ENCODERS_PAD_B { D4, D1 }
+
+
+/* LED layer indicators */
+#define LAYER_INDICATOR_LED_0 B6
+#define LAYER_INDICATOR_LED_1 B1
+#define LED_NUM_LOCK_PIN B3
+
+/* Bootmagic - hold down rotary encoder pushbutton while plugging in to enter bootloader */
+#define BOOTMAGIC_LITE_ROW 3
+#define BOOTMAGIC_LITE_COLUMN 0
+
+#define RGB_DI_PIN D3
+#ifdef RGB_DI_PIN
+    #define RGBLED_NUM 8
+    #define RGBLIGHT_HUE_STEP 8
+    #define RGBLIGHT_SAT_STEP 8
+    #define RGBLIGHT_VAL_STEP 8
+    #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+    #define RGBLIGHT_ANIMATIONS
+
+#endif
---
+++ keyboards/dumbpad/keymaps/impreza_vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB2, 0x85, 0x0F, 0x10, 0xA8, 0x8A, 0xE6, 0xE4}
+
