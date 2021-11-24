ffb0d75bff0f78951201e0bc671987ea697c4561
ambustion <ambustion@gmail.com>
2021-11-23 17:41:43 -0700

    Added security keys to keebio bdn9

--- keyboards/keebio/bdn9/keymaps/vial/config.h
+++ keyboards/keebio/bdn9/keymaps/vial/config.h
@@ -2,3 +2,8 @@
 
 #pragma once
 #define VIAL_KEYBOARD_UID {0x39, 0xA0, 0x58, 0xC7, 0x62, 0x1F, 0x33, 0x19}
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
