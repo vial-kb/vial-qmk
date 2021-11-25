0402c8e9dfc8ff848d09c524b928c2e99d35cc20
ll3macorn <gamertuber08@gmail.com>
2021-11-25 15:09:19 +0800

    kek
    kek

--- keyboards/bongopad/config.h
+++ keyboards/bongopad/config.h
@@ -12,17 +12,21 @@
 #define MANUFACTURER    Ll3macorn
 #define PRODUCT         BongoPad
 #define DESCRIPTION     BongoPad
+
+/* vial tings */
 #define VIAL_KEYBOARD_UID {0x08, 0xDB, 0x6F, 0x28, 0x40, 0xFD, 0x6F, 0x58}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
-#define NO_ACTION_TAPPING
-
-#define RGB_DI_PIN E6
-#define RGBLED_NUM 6
-
-
 #define VIAL_TAP_DANCE_ENTRIES 4
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+/* bootmagic */
+#define BOOTMAGIC_LITE_ROW 1
+#define BOOTMAGIC_LITE_COLUMN 0
+
+/* rgb underglow */
+#define RGB_DI_PIN E6
+#define RGBLED_NUM 6
 
 /* key matrix size */
 #define MATRIX_ROWS 4
@@ -33,36 +37,15 @@
 #define MATRIX_COL_PINS { F4, F5, F6 }
 #define UNUSED_PINS
 
-
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
+/* encoders */
 #define ENCODERS_PAD_A { B1 }
 #define ENCODERS_PAD_B { B3 }
+
+/* misc */
 #define LAYER_STATE_8BIT
-#define NO_ACTION_ONESHOT
-
-
-/* number of backlight levels */
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
-/* prevent stuck modifiers */
-#define PREVENT_STUCK_MODIFIERS
-
-
-
+#define NO_ACTION_TAPPING
 
 #endif
--- keyboards/bongopad/rules.mk
+++ keyboards/bongopad/rules.mk
@@ -42,19 +42,18 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 
 
 # Build Options
-#   comment out to disable the options.
-#
-BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
-CONSOLE_ENABLE = no	# Console for debug(+400)
-COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
+# comment out to disable the options.
+BOOTMAGIC_ENABLE = yes	
+MOUSEKEY_ENABLE = no	
+EXTRAKEY_ENABLE = yes	
+CONSOLE_ENABLE = no	
+COMMAND_ENABLE = no    
+SLEEP_LED_ENABLE = no  
+NKRO_ENABLE = yes
+BACKLIGHT_ENABLE = no  
 AUDIO_ENABLE = no
 ENCODER_ENABLE = yes
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
 WPM_ENABLE = yes
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no
