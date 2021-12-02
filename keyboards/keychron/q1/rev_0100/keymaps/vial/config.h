afa8952a5561b0b33312b1e3a597308de6a4869d
Teimor Epstein <31585013+teimor@users.noreply.github.com>
2021-12-03 01:20:53 +0200

    Update config.h

--- keyboards/keychron/q1/rev_0100/keymaps/vial/config.h
+++ keyboards/keychron/q1/rev_0100/keymaps/vial/config.h
@@ -2,6 +2,10 @@
 
 #pragma once
 
+
+//Vial Keyboard UID
+#define VIAL_KEYBOARD_UID {0x49, 0x5F, 0x9C, 0xB8, 0x0F, 0x67, 0x4D, 0xE6}
+
 /* key matrix pins */
 #define MATRIX_ROW_PINS { D3, D2, B3, B2, B1, B0 }
 #define MATRIX_COL_PINS { D5, D4, D6, D7, B4, B5, B6, C6, C7, F7, F6, F5, F4, F1, F0 }
@@ -15,5 +19,5 @@
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_MATRIX_KEYPRESSES
 
-//Vial Keyboard UID
-#define VIAL_KEYBOARD_UID {0x49, 0x5F, 0x9C, 0xB8, 0x0F, 0x67, 0x4D, 0xE6}
+#define VIAL_UNLOCK_ROWS { 0,2 }
+#define VIAL_UNLOCK_COLS { 0,13 }
