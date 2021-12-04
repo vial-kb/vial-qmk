7e85dbd746e8a4375a89f4c9abd256d5060ef819
Teimor Epstein <31585013+teimor@users.noreply.github.com>
2021-12-04 02:38:51 +0200

    remove unnecessary

--- keyboards/keychron/q1/rev_0100/keymaps/vial/config.h
+++ keyboards/keychron/q1/rev_0100/keymaps/vial/config.h
@@ -2,10 +2,6 @@
 
 #pragma once
 
-/* key matrix pins */
-#define MATRIX_ROW_PINS { D3, D2, B3, B2, B1, B0 }
-#define MATRIX_COL_PINS { D5, D4, D6, D7, B4, B5, B6, C6, C7, F7, F6, F5, F4, F1, F0 }
-
 //Vial Keyboard UID
 #define VIAL_KEYBOARD_UID {0x49, 0x5F, 0x9C, 0xB8, 0x0F, 0x67, 0x4D, 0xE6}
 
