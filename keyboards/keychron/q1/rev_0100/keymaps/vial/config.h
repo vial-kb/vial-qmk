dbee12d5c625a41a2d2f3350d92042f118d69280
Teimor Epstein <31585013+teimor@users.noreply.github.com>
2021-12-04 02:06:01 +0200

    enable rgb

--- keyboards/keychron/q1/rev_0100/keymaps/vial/config.h
+++ keyboards/keychron/q1/rev_0100/keymaps/vial/config.h
@@ -2,6 +2,15 @@
 
 #pragma once
 
+/* RGB Matrix Configuration */
+#define DRIVER_1_LED_TOTAL 59
+#define DRIVER_2_LED_TOTAL 23
+#define DRIVER_LED_TOTAL (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
+
+//enable RGB Matrix Effects for Vial
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
 //Vial Keyboard UID
 #define VIAL_KEYBOARD_UID {0x49, 0x5F, 0x9C, 0xB8, 0x0F, 0x67, 0x4D, 0xE6}
 
