27778b7cb8c78d39608749abd593f06dca2b9d6e
avianto <dev@avianto.com>
2021-12-09 01:24:39 -0500

    Add support for BabyV

---
+++ keyboards/eyeohdesigns/babyv/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0x59, 0x13, 0x81, 0x4D, 0x51, 0x6B, 0x12}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
---
+++ keyboards/eyeohdesigns/babyv/keymaps/vial/rules.mk
@@ -0,0 +1,5 @@
+BOOTMAGIC_ENABLE = lite
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
