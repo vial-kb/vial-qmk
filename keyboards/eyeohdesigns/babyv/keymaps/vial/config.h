8b0f74b63d589362cc1ac64a1b69963014f8b602
b. avianto <430395+avianto@users.noreply.github.com>
2021-12-09 19:37:42 -0500

    Add support for Eye Oh Designs' BabyV keyboard (#92)
    * Add support for BabyV
    
    * Add support for BabyV

---
+++ keyboards/eyeohdesigns/babyv/keymaps/vial/config.h
@@ -0,0 +1,25 @@
+/*
+Copyright 2020 Eye Oh! Designs
+Copyright 2021 avianto
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0x59, 0x13, 0x81, 0x4D, 0x51, 0x6B, 0x12}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
