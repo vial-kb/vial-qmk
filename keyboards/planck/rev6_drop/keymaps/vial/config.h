f3479b06165279161c3ffb84bcacfd02d8bf5890
dijidiji <6995669+dijidiji@users.noreply.github.com>
2022-01-19 15:31:29 +1100

    Planck rev6 drop (#105)
    * Planck rev6 support
    
    * Drop Planck rev6 RGB matrix support
    
    * Moved keymap to rev6_drop directory

---
+++ keyboards/planck/rev6_drop/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x69, 0xAE, 0x4D, 0xB7, 0x4A, 0x77, 0x5C, 0x95}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 5}
