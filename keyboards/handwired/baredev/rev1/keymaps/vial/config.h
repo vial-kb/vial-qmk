a977deb2b55118436dca6c625cac5916cbb23f93
Fernando Rodrigues <fernando.delphi@gmail.com>
2022-02-15 01:31:19 -0300

    Adding BareDev rev1 keyboard (#122)

---
+++ keyboards/handwired/baredev/rev1/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x73, 0x8C, 0x58, 0x53, 0xF8, 0x44, 0x5B, 0x0F}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
