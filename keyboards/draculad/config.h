ec8a6009e7364b2c82f6c802a1bff999955dddb1
doesntfazer <alexandermiller2525@gmail.com>
2022-03-15 09:57:04 -0700

    vial setup

--- keyboards/draculad/config.h
+++ keyboards/draculad/config.h
@@ -18,8 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include "config_common.h"
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x1B1E
+#define VENDOR_ID 0xFEEQ
+#define PRODUCT_ID 0x1K1D
 #define DEVICE_VER 0x0001
 #define MANUFACTURER mangoiv
 #define PRODUCT draculad
---
+++ keyboards/kb/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x21, 0xE5, 0xF5, 0xF2, 0x70, 0x7C, 0x37, 0xB5}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
