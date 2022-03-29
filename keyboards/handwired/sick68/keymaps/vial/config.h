fd783c8c5c0a83c6a6f3123ba7fd3fac6ab648be
xia0 <cpt.acky@gmail.com>
2022-03-29 22:32:17 +1030

    added vial port files

---
+++ keyboards/handwired/sick68/keymaps/vial/config.h
@@ -0,0 +1,26 @@
+/*
+Copyright 2022 @xia0
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#define ENCODERS_PAD_A { F0 }
+#define ENCODERS_PAD_B { F1 }
+#define ENCODER_RESOLUTION 4
+
+#define VIAL_KEYBOARD_UID {0x83, 0x5E, 0x9A, 0x67, 0xBE, 0xF8, 0xF1, 0xB8}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#define VIAL_ENCODER_DEFAULT { KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
---
+++ keyboards/handwired/sick68/keymaps/vial/rules.mk
@@ -0,0 +1,9 @@
+BOOTMAGIC_ENABLE = yes
+NKRO_ENABLE = yes
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+ENCODER_ENABLE = yes
+VIAL_ENCODERS_ENABLE = yes
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
