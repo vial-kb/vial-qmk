0e2a09710901f669a1ee5dcb4e71cc7da60f6225
Gabriel <gabeklav@gmail.com>
2022-04-01 00:15:31 -0400

    Add vial support for Savage65 PCB

---
+++ keyboards/cannonkeys/savage65/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE2, 0x7C, 0x05, 0x77, 0xB2, 0x41, 0xC6, 0x24}
+#define VIAL_UNLOCK_COMBO_ROWS { 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0 }
