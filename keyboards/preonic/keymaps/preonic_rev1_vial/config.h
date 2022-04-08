958d3759a7c01db3c4ed5e9570375e762300a111
Jim Erickson <jimerickso@gmail.com>
2022-04-07 20:38:14 -0500

    fixed config.h define VIAL_UNLOCK

--- keyboards/preonic/keymaps/preonic_rev1_vial/config.h
+++ keyboards/preonic/keymaps/preonic_rev1_vial/config.h
@@ -33,3 +33,5 @@
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
 #define VIAL_KEYBOARD_UID {0xAF, 0x13, 0x31, 0x67, 0xE5, 0xBC, 0x4E, 0xF6}
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
