9c80c07824c1eb66661ce7c9bf44d67f654761ae
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-04-08 19:42:23 -0700

    Fixed unlock combo coordinates

--- keyboards/cantor/keymaps/vial/config.h
+++ keyboards/cantor/keymaps/vial/config.h
@@ -2,8 +2,8 @@
 /* keyboard uid */
 #define VIAL_KEYBOARD_UID {0xF0, 0x67, 0x2F, 0x54, 0x23, 0xB3, 0x7B, 0x55}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 4, 5 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
 
 /* default layer count */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
