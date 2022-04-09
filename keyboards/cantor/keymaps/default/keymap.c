13459965889e8e4b2c0a5a5e0fe37733c07cda57
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-04-09 16:07:12 -0700

    Fixed rules.mk

--- keyboards/cantor/keymaps/default/keymap.c
+++ keyboards/cantor/keymaps/default/keymap.c
@@ -23,25 +23,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                             KC_LGUI, KC_SPC, KC_BSPC,          KC_ENT, KC_SPC, KC_RALT
-    ),
-
-    [1] = LAYOUT_split_3x6_3(
-        KC_TRANS,  KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,
-        KC_TRANS, KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS, KC_TRANS,
-        KC_TRANS, KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS, KC_TRANS,  KC_TRANS, KC_TRANS,
-                                            KC_TRANS, KC_TRANS, KC_TRANS,          KC_TRANS, KC_TRANS, KC_TRANS
-    ),
-    [2] = LAYOUT_split_3x6_3(
-        KC_TRANS,  KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,
-        KC_TRANS, KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS, KC_TRANS,
-        KC_TRANS, KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS, KC_TRANS,  KC_TRANS, KC_TRANS,
-                                            KC_TRANS, KC_TRANS, KC_TRANS,          KC_TRANS, KC_TRANS, KC_TRANS
-    ),
-    [3] = LAYOUT_split_3x6_3(
-        KC_TRANS,  KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,
-        KC_TRANS, KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS, KC_TRANS,
-        KC_TRANS, KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,    KC_TRANS,                               KC_TRANS,    KC_TRANS,    KC_TRANS, KC_TRANS,  KC_TRANS, KC_TRANS,
-                                            KC_TRANS, KC_TRANS, KC_TRANS,          KC_TRANS, KC_TRANS, KC_TRANS
     )
 };
 
--- keyboards/cantor/rules.mk
+++ keyboards/cantor/rules.mk
@@ -1,5 +1,3 @@
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = usart
-VIA_ENABLE      = yes    # Enable VIA
-VIAL_ENABLE      = yes    # Enable VIAL
