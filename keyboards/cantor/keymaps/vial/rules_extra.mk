dba10ced059a5d1f7eb6605cc2dab5eb2b97e153
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-04-08 19:46:35 -0700

    Removed extra rules file

--- keyboards/cantor/keymaps/vial/rules_extra.mk
+++
@@ -1,7 +0,0 @@
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-
-SPLIT_KEYBOARD = yes
-SERIAL_DRIVER = usart
-#VIA_ENABLE      = yes    # Enable VIA
-VIAL_ENABLE      = yes    # Enable VIA
-VIAL_INSECURE    = yes
--- keyboards/cantor/rules.mk
+++ keyboards/cantor/rules.mk
@@ -2,4 +2,4 @@ BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = usart
 VIA_ENABLE      = yes    # Enable VIA
-VIAL_ENABLE      = yes    # Enable VIA
+VIAL_ENABLE      = yes    # Enable VIAL
