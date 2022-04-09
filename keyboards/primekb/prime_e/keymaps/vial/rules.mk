3e3f5c433e9b138fba6e7bf322e4764e74406e07
Zhol Internet <zhol@internet.keyboard>
2022-04-09 02:32:58 -0700

    Arisu, prime_e, ymd75 vial support

---
+++ keyboards/primekb/prime_e/keymaps/vial/rules.mk
@@ -0,0 +1,10 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+# DYNAMIC_KEYMAP_LAYER_COUNT in prime_e/config.h is set to 8
+# reduce that to re-enable these settings
+QMK_SETTINGS = no
+COMBO_ENABLE = no
---
+++ keyboards/ymdk/ymd75/keymaps/vial/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+KEY_LOCK_ENABLE = no
+COMBO_ENABLE = no
