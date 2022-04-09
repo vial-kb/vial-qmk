d2dac77228fd034a2cb7e21de7120feb31437f86
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-04-08 19:36:08 -0700

    Porting Cantor

---
+++ keyboards/cantor/keymaps/vial/rules_extra.mk
@@ -0,0 +1,7 @@
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = usart
+#VIA_ENABLE      = yes    # Enable VIA
+VIAL_ENABLE      = yes    # Enable VIA
+VIAL_INSECURE    = yes
