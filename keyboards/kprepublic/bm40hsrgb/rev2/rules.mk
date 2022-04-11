5e4bed58d8e18a19abb585a140b50a77ce8e2344
Jim Erickson <jimerickso@gmail.com>
2022-04-11 14:53:35 -0500

    put rev1 files in rev1 folder and add rev2 folder and files

---
+++ keyboards/kprepublic/bm40hsrgb/rev2/rules.mk
@@ -0,0 +1,23 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes        # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes          # Mouse keys
+EXTRAKEY_ENABLE = yes          # Audio control and System control
+CONSOLE_ENABLE = no            # Console for debug
+COMMAND_ENABLE = no            # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes           # Enable keyboard RGB underglow
+MIDI_ENABLE = no               # MIDI support
+AUDIO_ENABLE = no              # Audio output on port C6
+RGB_MATRIX_ENABLE = yes        # Use RGB matrix
+RGB_MATRIX_DRIVER = IS31FL3733
+LAYOUTS = planck_mit
+LTO_ENABLE = yes
