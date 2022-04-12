018d6f33f3d90e8551819317bc53a7130f0abbbf
chance <chance@trashcat.xyz>
2022-04-11 23:09:12 -0400

    Add files via upload

---
+++ keyboards/4pplet/waffling60/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4A, 0x24, 0xC6, 0x5E, 0x42, 0x4F, 0x90, 0x7D}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define VIAL_TAP_DANCE_ENTRIES 4
+
