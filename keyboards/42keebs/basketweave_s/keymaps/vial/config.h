d744074de2e8303479fcc5e164867cd6909311c9
Gerrit Plehn <52218643+GerritPlehn@users.noreply.github.com>
2022-04-26 10:25:22 +0200

    VIAL support for basketweave (#149)
    * VIAL support for basketweave
    
    * fix: bootmagic from lite to yes

---
+++ keyboards/42keebs/basketweave_s/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x82, 0x1C, 0x92, 0xC7, 0x6B, 0xC1, 0xB2, 0xA9}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+
+#define VIAL_ENCODER_DEFAULT { KC_VOLU, KC_VOLD, KC_MS_WH_UP, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
