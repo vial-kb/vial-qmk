930f38f132bb65b29f4bf482956e2b8f5b73993c
eugenepy <tn00372136@gmail.com>
2022-05-04 12:06:41 +0800

    update vial-configs

---
+++ keyboards/ekow/akira/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD6, 0xF4, 0x6F, 0x56, 0x82, 0xE8, 0x06, 0x7B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
--- keyboards/ekow/akira/rules.mk
+++ keyboards/ekow/akira/rules.mk
@@ -5,8 +5,7 @@ MCU = STM32F042
 BOOTLOADER = stm32-dfu
 
 # Build Options
-#   change yes to no to disable
-#
+# change yes to no to disable
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
