364f61e25d6a82a1b2a6ba7a73b95e619609ffcf
eugenepy <tn00372136@gmail.com>
2022-05-07 19:11:00 +0800

    resolve conflicts

--- keyboards/ekow/akira/keymaps/vial/rules.mk
+++ keyboards/ekow/akira/keymaps/vial/rules.mk
@@ -1,18 +1,4 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
-
-<<<<<<< HEAD
-LTO_ENABLE = yes
-
-=======
 LTO_ENABLE = no
 QMK_SETTINGS = no
-
-MOUSEKEY_ENABLE = no		# Mouse keys
-EXTRAKEY_ENABLE = no		# Audio control and System control
-
-NKRO_ENABLE = no 			# Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
->>>>>>> e1ed58464c650374e2d1c8d8cdd24d2c4d5e3a39
