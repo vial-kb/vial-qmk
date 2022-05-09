33f8afbf84e48d761c121e8085fd18a6b02f37f6
crkochan <5183896+crkochan@users.noreply.github.com>
2022-05-08 22:38:37 -0700

    Added a shift+alt+tab function

--- keyboards/geekboards/macropad_v2/keymaps/vial/keymap.c
+++ keyboards/geekboards/macropad_v2/keymaps/vial/keymap.c
@@ -21,6 +21,7 @@ uint16_t alt_tab_timer = 0;
 
 enum custom_keycodes {
   ALT_TAB = USER00,
+  SHIFT_ALT_TAB = USER01,
 };
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,7 +38,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //------------ SUPER ALTTAB ---------------
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {               
+    switch (keycode) {
         case ALT_TAB:
             if (record->event.pressed) {
                 if (!is_alt_tab_active) {
@@ -50,14 +51,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_TAB);
             }
             break;
+        case SHIFT_ALT_TAB:
+            if (record->event.pressed) {
+                if (!is_alt_tab_active) {
+                    is_alt_tab_active = true;
+                    register_code(KC_LALT);
+                    register_code(KC_LSFT);
+                }
+                alt_tab_timer = timer_read();
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_TAB);
+            }
+            break;
     }
     return true;
 }
 
-void matrix_scan_user(void) {     
+void matrix_scan_user(void) {
     if (is_alt_tab_active) {
         if (timer_elapsed(alt_tab_timer) > 1000) {
             unregister_code(KC_LALT);
+            unregister_code(KC_LSFT);
             is_alt_tab_active = false;
         }
     }
