d19ba2b298eaf49536a20f1f806e5036fb3ed406
JuanLu <juanlufont@gmail.com>
2022-05-13 00:26:38 +0200

    Add Vial support for GH60 Satan

---
+++ keyboards/gh60/satan/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x32, 0x5E, 0xC4, 0x12, 0x07, 0x3B, 0xC8, 0x83}
+
+// ESC + Enter
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
