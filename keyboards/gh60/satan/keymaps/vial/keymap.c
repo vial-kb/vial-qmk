06f0ae96b7efe9d38a42a2d0fea794e53d4c486c
JuanLu <juanlufont@gmail.com>
2022-05-13 01:50:44 +0200

    Remove unnecessary comma

--- keyboards/gh60/satan/keymaps/vial/keymap.c
+++ keyboards/gh60/satan/keymaps/vial/keymap.c
@@ -32,5 +32,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
+    )
 };
