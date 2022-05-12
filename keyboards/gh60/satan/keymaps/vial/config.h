1fadc88822a6921b970d8400714e8c0a1f79bfc5
JuanLu <juanlufont@gmail.com>
2022-05-13 01:49:59 +0200

    Disable options to reduce firmware size

--- keyboards/gh60/satan/keymaps/vial/config.h
+++ keyboards/gh60/satan/keymaps/vial/config.h
@@ -3,6 +3,8 @@
 
 #define VIAL_KEYBOARD_UID {0x32, 0x5E, 0xC4, 0x12, 0x07, 0x3B, 0xC8, 0x83}
 
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+
 // ESC + Enter
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
