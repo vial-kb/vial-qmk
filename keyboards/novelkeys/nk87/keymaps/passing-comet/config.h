9fa0c6ff90b8647a7b0e51ea13050adeb99f0512
passing comet <5461413+passing-comet@users.noreply.github.com>
2022-06-05 22:49:00 -0400

    implement vial for nk87

---
+++ keyboards/novelkeys/nk87/keymaps/passing-comet/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9C, 0x04, 0x30, 0x89, 0xCC, 0x5A, 0xBD, 0x5A}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#define VIAL_TAP_DANCE_ENTRIES 16
+
+#define DEBOUNCE 4
+#define TAPPING_TERM 175
