8c8d3c6f3df36dcd1014bdad2fb38713d7305cd2
Jan Lindblom <jan@namnlos.io>
2022-06-16 15:40:42 +0300

    Put BACKLIGHT_BREATHING at keyboard level.

--- keyboards/b_sides/rev41lp/config.h
+++ keyboards/b_sides/rev41lp/config.h
@@ -35,6 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BACKLIGHT_PIN B6
 #define BACKLIGHT_ON_STATE 1
+#define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 7
 #define BACKLIGHT_LIMIT_VAL 150
 
