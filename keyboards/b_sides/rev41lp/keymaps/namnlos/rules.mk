27f7e1006bd25b8dc86bc0bb7aace1ba35ca8829
Jan Lindblom <jan@namnlos.io>
2022-06-17 01:03:04 +0300

    Something's not quite right with backlight breathing, was working perfectly fine using the same src with latest qmk...

--- keyboards/b_sides/rev41lp/keymaps/namnlos/rules.mk
+++ keyboards/b_sides/rev41lp/keymaps/namnlos/rules.mk
@@ -1,16 +1,18 @@
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = no        # Mouse keys, not used.
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_DRIVER = pwm      # Hardware PWM backlight driver.
 UNICODE_ENABLE = no
 UNICODEMAP_ENABLE = yes
-CONSOLE_ENABLE = no
 DEBOUNCE_TYPE = sym_defer_pk
 TAP_DANCE_ENABLE = yes
-COMBO_ENABLE = no
+
+COMBO_ENABLE = no           # Not using combos.
 GRAVE_ESC_ENABLE = no
 SPACE_CADET_ENABLE = no
-VERBOSE = no
 RGBLIGHT_ENABLE = no
 AUDIO_ENABLE = no
+VERBOSE = no
+CONSOLE_ENABLE = no
 
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
