b31ccc31ce944af7503b3665153521cf8f67416f
brujoand <anders@brujordet.no>
2022-06-25 11:53:37 +0200

    Adding vial support to iris keyboards

---
+++ keyboards/keebio/iris/keymaps/vial/config.h
@@ -0,0 +1,29 @@
+/*
+Copyright 2017 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// #define USE_I2C
+#define EE_HANDS
+
+#define VIAL_KEYBOARD_UID {0x9D, 0xA3, 0xB2, 0xE1, 0x78, 0x1D, 0xDC, 0xB9}
+
+
+// define Vial unlock as escape and lower left modifier
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
