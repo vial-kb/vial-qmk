81a9b07b6500fb1a0887433e05aad55eb19b2e85
WrinkleKrinkles <100941474+WrinkleKrinkles@users.noreply.github.com>
2022-07-01 02:59:57 +0000

    Add VIAL support for Ninjin TKL
    Build file 49502 bytes.
    
    No features were removed; firmware was tested on ANSI, F12, WK layout.

---
+++ keyboards/xelus/ninjin/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1D, 0x56, 0xD2, 0xE3, 0xFF, 0x00, 0x39, 0x52}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
+
