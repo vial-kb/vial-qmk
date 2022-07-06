6a37d8aec30854db01e291ae05ce355533cc785c
ardent-handsaw <102997168+ardent-handsaw@users.noreply.github.com>
2022-07-06 10:40:05 -0500

    Add Vial support to Rama Works M10-A macropad (#1)
    * initial M10a attempt
    
    Building VIAL support for Rama M10-A based on VIA build. Not working
    
    * Added VIAL support for Rama Works M10-A macropad
    
    * Add VIAL support for Rama Works M10-A macropad

--- keyboards/m10a/config.h
+++ keyboards/m10a/config.h
@@ -25,7 +25,7 @@
 #define PRODUCT_ID      0x00AA // 10-A
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    RAMA WORKS
-#define PRODUCT         RAMA WORKS M10-A
+#define PRODUCT         M10-A
 
 /* key matrix size */
 #define MATRIX_ROWS 4
