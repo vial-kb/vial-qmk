3ced18d71f647bb46897fd7dea485ffaa933a84e
Niko Wenselowski <niko@nerdno.de>
2022-07-07 12:05:29 +0200

    Use right-most key

--- keyboards/ut472/keymaps/vial/config.h
+++ keyboards/ut472/keymaps/vial/config.h
@@ -5,4 +5,4 @@
 #define VIAL_KEYBOARD_UID {0xCE, 0xA7, 0x01, 0xED, 0x54, 0x99, 0x01, 0x18}
 
 #define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
