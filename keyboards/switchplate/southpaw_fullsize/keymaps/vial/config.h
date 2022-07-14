907d2edb69c48e78605a61515072a04b1d98ad4d
a33bis <yetanotheraccount@mailbox.org>
2022-07-14 11:56:38 -0400

    Fixing missing row for locking function

--- keyboards/switchplate/southpaw_fullsize/keymaps/vial/config.h
+++ keyboards/switchplate/southpaw_fullsize/keymaps/vial/config.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xDD, 0x01, 0x0B, 0xFC, 0x91, 0x28, 0xD7, 0x3E}
-#define VIAL_UNLOCK_COMBO_ROWS {0, 5}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 5, 5}
 #define VIAL_UNLOCK_COMBO_COLS {0, 19, 21}
 
 #define BACKLIGHT_BREATHING
