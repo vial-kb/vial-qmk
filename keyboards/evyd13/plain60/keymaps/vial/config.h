2fbe88385334c6543200c66a6d6e3886f29d64b1
a33bis <48868985+a33bis@users.noreply.github.com>
2022-07-13 20:20:13 -0400

    Support for evyd13/plain60 (#198)
    * AVR error workaround
    
    * Adding vial support to evyd13/plain60
    
    * Revert "AVR error workaround"
    
    This reverts commit 3eb283de6558d394ba16af813cd361b75b6026a8.

---
+++ keyboards/evyd13/plain60/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xED, 0x26, 0xCA, 0x6E, 0xC6, 0x33, 0x93, 0x24}
+#define VIAL_UNLOCK_COMBO_ROWS {1, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 6}
