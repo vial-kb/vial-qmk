205889dae5b82f4abc2d9d3965580de5142a6507
a33bis <yetanotheraccount@mailbox.org>
2022-07-14 11:53:33 -0400

    Created vial/config.h

---
+++ keyboards/switchplate/southpaw_fullsize/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDD, 0x01, 0x0B, 0xFC, 0x91, 0x28, 0xD7, 0x3E}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 5}
+#define VIAL_UNLOCK_COMBO_COLS {0, 19, 21}
+
+#define BACKLIGHT_BREATHING
