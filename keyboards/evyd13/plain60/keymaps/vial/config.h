3fa971ca41ff33b59df7efa9fb2ff15d51a9a111
a33bis <48868985+a33bis@users.noreply.github.com>
2022-07-16 15:53:02 -0400

    Plain60: Fix conflict between Bootmagic Lite and qmk-dfu (#206)
    * Workaround for dfu/bootmagic conflict + support for nkro and mouse keys
    
    * Changed Bootmagic key to Left Control

--- keyboards/evyd13/plain60/keymaps/vial/config.h
+++ keyboards/evyd13/plain60/keymaps/vial/config.h
@@ -5,3 +5,8 @@
 #define VIAL_KEYBOARD_UID {0xED, 0x26, 0xCA, 0x6E, 0xC6, 0x33, 0x93, 0x24}
 #define VIAL_UNLOCK_COMBO_ROWS {1, 4}
 #define VIAL_UNLOCK_COMBO_COLS {0, 6}
+
+/* Moves Bootmagic Lite key from "Esc" to "Left Control" to avoid conflict with qmk-dfu
+ * See https://docs.qmk.fm/#/flashing?id=qmk-dfu */
+#define BOOTMAGIC_LITE_ROW 4
+#define BOOTMAGIC_LITE_COLUMN 0
