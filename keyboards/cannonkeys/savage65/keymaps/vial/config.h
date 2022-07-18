1d327a24f95d16b2a455bd935ebab2cca7324b11
Gabriel Klavans <gabeklav@fastmail.com>
2022-07-18 07:49:33 -0700

    Add second key to unlock combo

--- keyboards/cannonkeys/savage65/keymaps/vial/config.h
+++ keyboards/cannonkeys/savage65/keymaps/vial/config.h
@@ -3,5 +3,5 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xE2, 0x7C, 0x05, 0x77, 0xB2, 0x41, 0xC6, 0x24}
-#define VIAL_UNLOCK_COMBO_ROWS { 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
