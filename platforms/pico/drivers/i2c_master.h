d5e9eb37d906807e40f6d60f85914b5b0a70f1d0
Johannes Schneider <johannes.schneider@leica-geosystems.com>
2022-08-04 10:51:04 +0200

    pico: i2c: align code with chibios, avr, documentation
    some defines and function signatures did not quite match the
    documented ones - which lead to compile issues while building the same
    code that uses an i2c-driver based vial-qmk/driver for multiple
    platforms
    
    Signed-off-by: Johannes Schneider <johschneider@googlemail.com>

--- platforms/pico/drivers/i2c_master.h
+++ platforms/pico/drivers/i2c_master.h
@@ -26,26 +26,20 @@
 
 #include <stdint.h>
 
-#ifndef I2C1_SCL
-#    define I2C1_SCL 3
-#endif
-#ifndef I2C1_SDA
-#    define I2C1_SDA 2
-#endif
-
-#ifndef I2C_DRIVER
-#    define I2C_DRIVER i2c1
-#endif
-
 typedef int16_t i2c_status_t;
 
 #define I2C_STATUS_SUCCESS (0)
 #define I2C_STATUS_ERROR (-1)
 #define I2C_STATUS_TIMEOUT (-2)
 
+/* collides with:
+ * pico-sdk/src/rp2_comon/hardware_i2c/include/hardware/i2c.h::i2c_init
+ * hence the bodge...
+ */
 #define i2c_init() i2c_init_pico()
 void         i2c_init_pico(void);
-i2c_status_t i2c_start(uint8_t address);
+
+i2c_status_t i2c_start(uint8_t address, uint16_t timeout);
 i2c_status_t i2c_transmit(uint8_t address, const uint8_t* data, uint16_t length, uint16_t timeout);
 i2c_status_t i2c_receive(uint8_t address, uint8_t* data, uint16_t length, uint16_t timeout);
 i2c_status_t i2c_writeReg(uint8_t devaddr, uint8_t regaddr, const uint8_t* data, uint16_t length, uint16_t timeout);
