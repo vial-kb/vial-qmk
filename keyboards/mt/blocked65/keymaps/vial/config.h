798a989964414912eddd755261e9f5ffb48a444d
Alberto <albertopastrolin@gmail.com>
2022-08-11 14:53:24 +0200

    feat: ported to vial

---
+++ keyboards/mt/blocked65/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC3, 0xA2, 0x64, 0xDD, 0xCD, 0x9E, 0x6C, 0xC9}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
