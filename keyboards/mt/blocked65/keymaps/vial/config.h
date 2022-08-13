b4bce40ec851ba226a7532b88cba491c46338f1c
Alberto <albertopastrolin@gmail.com>
2022-08-13 14:43:33 +0200

    fix: typo fix

--- keyboards/mt/blocked65/keymaps/vial/config.h
+++ keyboards/mt/blocked65/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0xC3, 0xA2, 0x64, 0xDD, 0xCD, 0x9E, 0x6C, 0xC9}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
