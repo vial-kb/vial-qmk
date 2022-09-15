c510ac4ba7dc1095fce9808d66a3ff7083fd694e
honorless <86894501+lesshonor@users.noreply.github.com>
2022-09-14 20:57:28 -0400

    post-breaking changes refactor

--- keyboards/mechwild/mokulua/mirrored/keymaps/vial/rules.mk
+++ keyboards/mechwild/mokulua/mirrored/keymaps/vial/rules.mk
@@ -1,9 +1,11 @@
 VIA_ENABLE           = yes
-VIAL_ENABLE          = yes
-VIAL_ENCODERS_ENABLE = yes
-
 LTO_ENABLE           = yes
-QMK_SETTINGS         = no
-KEY_OVERRIDE_ENABLE  = no
-TAP_DANCE_ENABLE     = no
-COMBO_ENABLE         = no
+VIAL_ENABLE          = yes
+
+ifeq ($(strip $(MCU)), atmega32u4)
+    QMK_SETTINGS         = no
+
+    COMBO_ENABLE         = no
+    KEY_OVERRIDE_ENABLE  = no
+    TAP_DANCE_ENABLE     = no
+endif
