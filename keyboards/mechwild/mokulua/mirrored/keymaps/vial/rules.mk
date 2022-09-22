2d0c8d8933764db339c383a7c5c55efdde3bd6a9
honorless <86894501+lesshonor@users.noreply.github.com>
2022-09-21 23:45:45 -0400

    fix: enable encoder mapping

--- keyboards/mechwild/mokulua/mirrored/keymaps/vial/rules.mk
+++ keyboards/mechwild/mokulua/mirrored/keymaps/vial/rules.mk
@@ -2,9 +2,12 @@ VIA_ENABLE           = yes
 LTO_ENABLE           = yes
 VIAL_ENABLE          = yes
 
+ENCODER_MAP_ENABLE   = yes
+
 ifeq ($(strip $(MCU)), atmega32u4)
     QMK_SETTINGS         = no
 
+	MAGIC_ENABLE         = no
     COMBO_ENABLE         = no
     KEY_OVERRIDE_ENABLE  = no
     TAP_DANCE_ENABLE     = no
