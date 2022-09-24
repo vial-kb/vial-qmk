4181a3499819e1ae7320445448a2b89c1dfdd0dc
kevinmgee <kevingee@kevingee.biz>
2022-09-24 18:15:37 -0400

    Porting the mini42 to Vial for the first time.

---
+++ keyboards/controllerworks/mini42/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x08, 0xE9, 0xFB, 0x54, 0x10, 0x3E, 0xE0, 0x63}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+#define VIALRGB_NO_DIRECT
