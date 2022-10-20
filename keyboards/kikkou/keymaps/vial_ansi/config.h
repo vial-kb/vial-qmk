8dca3a5e9229728852192b262da2e6ef47bdd75b
tipsycod3r <68339225+tipsycod3r@users.noreply.github.com>
2022-10-20 16:12:52 +0800

    Added vial_ansi for Kikkou
    Ported kikkou ansi layout to vial.

---
+++ keyboards/kikkou/keymaps/vial_ansi/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB1, 0x97, 0x73, 0x5C, 0x00, 0xB8, 0x2E, 0xF9}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
