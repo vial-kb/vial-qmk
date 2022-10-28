79ae6362ebe32043f10c5c749dd3774b6f788930
Jakob Grønhaug <jakob@gronha.ug>
2022-10-28 22:43:10 +0200

    Remove references to unused layout

--- keyboards/libra_mini/config.h
+++ keyboards/libra_mini/config.h
@@ -4,10 +4,10 @@
 
 /* Matrix config */
 
-#define MATRIX_ROWS 5
+#define MATRIX_ROWS 4
 #define MATRIX_COLS 12
 
-#define MATRIX_ROW_PINS { E6, B4, B5, B2, NO_PIN }
+#define MATRIX_ROW_PINS { E6, B4, B5, B2 }
 #define MATRIX_COL_PINS { B3, B1, F7, F6, F5, F4, C6, D4, D0, D1, D2, D3 }
 
 #define DIODE_DIRECTION ROW2COL
