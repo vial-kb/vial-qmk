613b0351be9af87d0c862365370b641c0d6b1d14
One-Nub <38899321+One-Nub@users.noreply.github.com>
2022-11-03 01:45:21 -0500

    Move Vial specific configs back to keymap config

--- keyboards/adafruit/macropad/config.h
+++ keyboards/adafruit/macropad/config.h
@@ -18,10 +18,6 @@
 
 #include "config_common.h"
 
-#define VIAL_KEYBOARD_UID {0xFB, 0xDF, 0x01, 0xD7, 0x67, 0x0D, 0x4B, 0x54}
-#define VIAL_UNLOCK_COMBO_ROWS { 1, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 3
