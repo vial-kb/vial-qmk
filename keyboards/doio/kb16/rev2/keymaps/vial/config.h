494e193ce44ff3416b891fc4189c88a00e1087d1
Gerrit Plehn <gerritplehn@gmail.com>
2022-11-13 11:58:37 +0100

    doio/kb16 vial

---
+++ keyboards/doio/kb16/rev2/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x7E, 0x74, 0x4A, 0x72, 0xB1, 0xC6, 0x55, 0xC6}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
