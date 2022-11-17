b8005a83f79924d1741dc1745f97dd3dc9448965
Pastitas <Pastitas@users.noreply.github.com>
2022-11-17 04:27:17 +0100

    Redox_w port (#286)
    * Add support for redox_w
    
    * Add redox support
    
    * change uid and delete dupe
    
    * fix manufacturer
    
    * Revert to old redox UID
    
    * Changed rules to recover qmk settings
    
    Co-authored-by: Alejandro Bolullo <alejandro.bolullo@trustyou.net>

---
+++ keyboards/redox_w/keymaps/vial/rules.mk
@@ -0,0 +1,15 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+
+COMMAND_ENABLE = no			# Commands for debug and configuration
+RGBLIGHT_ENABLE = no		# Enable WS2812 RGB underlight.
+UNICODE_ENABLE = no
+OLED_DRIVER_ENABLE = no
+CONSOLE_ENABLE = no			# Console for debug
+MUSIC_ENALBE = no
+SPACE_CADET_ENABLE = no
+
+# must be disabled for VIA support
+MIDI_ENABLE = no
+BLUETOOTH_ENABLE = no
