5163b6c140969bc901cd8443b7585c1e0bfcb60f
John Hall <jhall@alum.wpi.edu>
2022-11-24 23:21:44 -0500

    Added support for Velocifire Sun20pro numpad

---
+++ keyboards/velocifire/sun20pro/sun20pro.h
@@ -0,0 +1,49 @@
+/* Copyright 2022 John Hall
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "quantum.h"
+
+#define XXX KC_NO
+
+/* maypad numpad layout
+ * .-------------------.
+ * |NLCK|   /|   *|   -|
+ * |-------------------|
+ * |   7|   8|   9|    |
+ * |--------------|    |
+ * |   4|   5|   6|   +|
+ * |-------------------|
+ * |   1|   2|   3|    |
+ * |--------------|    |
+ * |        0|   .| Ent|
+ * '-------------------'
+ */
+// The first section contains all of the arguments
+// The second converts the arguments into a two-dimensional array
+#define LAYOUT( \
+    K00, K01, K02, K03, K33, \
+    K10, K11, K12,      \
+    K20, K21, K22, K23, \
+    K30, K31, K32, \
+    K40,      K42, K43  \
+) { \
+    { K00, K01, K02, K03 }, \
+    { K10, K11, K12, XXX }, \
+    { K20, K21, K22, K23 }, \
+    { K30, K31, K32, K33 }, \
+    { K40, XXX, K42, K43 }  \
+}
