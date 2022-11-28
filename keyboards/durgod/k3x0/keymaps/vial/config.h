722a28f221694403909822f4a3131d18f5790d84
Ben Clarke <ben@arrayofstars.com>
2022-11-27 21:09:47 -0500

    [Keymap] Add vial keymap for Durgod K320

---
+++ keyboards/durgod/k3x0/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD9, 0x7E, 0x0A, 0x00, 0x7B, 0x97, 0x22, 0x3B}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
