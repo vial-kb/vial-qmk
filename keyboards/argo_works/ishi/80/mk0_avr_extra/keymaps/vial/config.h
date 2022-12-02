76d19050ecfe01d791c68ec4c92ef7f3acd1d069
jasonj2232 <40370113+jasonj2232@users.noreply.github.com>
2022-12-03 05:08:37 +0530

    [Keyboard] Added support for a new keyboard - Ishi 80 Mk0 (#297)
    * Initial VIAL support added
    
    * Enabled encoder support
    
    * Removed a tags.Removed encoder support from non extra avr
    
    * Fixed code that deviated from QMK guidelines and requirements
    
    * Changed integer defines to enums
    
    * Changed the Keyboard UID to be unique

---
+++ keyboards/argo_works/ishi/80/mk0_avr_extra/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x48, 0x54, 0x52, 0xF6, 0x51, 0x5B, 0xAB, 0x02}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 8, 8 }
