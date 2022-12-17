b9e3339f22b7ac3bdb2032934ed3ff7190d2d34e
laidbackhacker <70282382+JW2586@users.noreply.github.com>
2022-12-17 18:58:16 +0000

    waterfowl support

---
+++ keyboards/waterfowl/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x95, 0x71, 0x9F, 0x29, 0xA6, 0x0B, 0x39, 0x57}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2, 4, 6}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0, 0, 0}
