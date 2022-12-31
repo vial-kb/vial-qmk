0b207f9f7a79e0475945a20e2f61823d6b7cc734
adophoxia <andyao1528@gmail.com>
2022-12-31 14:16:58 -0800

    Port VIAL to Keychron Q9

---
+++ keyboards/keychron/q9/ansi/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x60, 0xB4, 0x3E, 0xDF, 0x73, 0x78, 0x8B, 0x56}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
