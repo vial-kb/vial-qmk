ee35edd7a6ec24ee66d18cbfacb5fe8ed17e6c0f
adophoxia <andyao1528@gmail.com>
2023-01-01 09:31:35 -0800

    Port Keychron Q6 to VIAL

---
+++ keyboards/keychron/q6/iso_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFD, 0x58, 0xBA, 0x86, 0x08, 0xC1, 0x3D, 0x5E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
