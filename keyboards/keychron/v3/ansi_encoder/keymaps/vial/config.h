98acd6b0b6a082901c9b83be87c5e3cd29ac090e
adophoxia <andyao1528@gmail.com>
2023-01-02 16:53:23 -0800

    Port Keychron V3 to VIAL

---
+++ keyboards/keychron/v3/ansi_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3E, 0xD9, 0x53, 0x3C, 0x7F, 0xCC, 0x10, 0x02}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
