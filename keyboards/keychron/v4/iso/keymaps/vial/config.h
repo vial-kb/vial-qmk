beda4a114f27eb41fcf57e1ec3fd6848d9e356d2
adophoxia <andyao1528@gmail.com>
2023-01-02 17:09:44 -0800

    Port Keychron V4 to VIAL

---
+++ keyboards/keychron/v4/iso/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE1, 0x1F, 0xE6, 0x47, 0x42, 0x90, 0xC8, 0xB5}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
