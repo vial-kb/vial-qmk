6b0c422538b421d0f206a52590f0e46bfc75a043
Ross Huelin <filmstarr@hotmail.com>
2023-01-06 14:53:08 +0000

    Add Doio kb04 keyboard.

---
+++ keyboards/doio/kb04/keymaps/vial-inverted/config.h
@@ -0,0 +1,19 @@
+/* Copyright 2022 filmstarr <https://github.com/filmstarr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x11, 0x10, 0xF5, 0xD0, 0x98, 0x20, 0xE9, 0xF6}
---
+++ keyboards/doio/kb04/keymaps/vial-inverted/rules.mk
@@ -0,0 +1,6 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+
+# Encoder enabled
+ENCODER_MAP_ENABLE = yes
