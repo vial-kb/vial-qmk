dde1982482af78ed29504cd3088c8c4c19ccf89d
Andres Sedano <6133759+AndySedano@users.noreply.github.com>
2023-01-07 15:10:57 -0800

    FIX Keychron_q0 unlock keys
    Remapped the enter key to unlock the keyboard

--- keyboards/keychron/q0/rev_0130/keymaps/vial/config.h
+++ keyboards/keychron/q0/rev_0130/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0xCD, 0xB9, 0xD4, 0xC9, 0x53, 0x9A, 0x68, 0xC3}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
