5b95a75faca7d6bb0940e1108f53280c3552871f
pedrorq <37935556+pedrorq@users.noreply.github.com>
2023-01-11 11:02:03 +0000

    VIAL firmware for winry315
    First attempt

---
+++ keyboards/winry/winry315/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x22, 0xC3, 0x6B, 0x95, 0x11, 0x79, 0x7F, 0x80}
+
+// top left and bottom left corners when encoders are north
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 0 }
+
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
---
+++ keyboards/winry/winry315/keymaps/vial/rules.mk
@@ -0,0 +1,7 @@
+VIAL_ENABLE = yes
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+QMK_SETTINGS = no
+
+
+SRC += encoder_actions.c
