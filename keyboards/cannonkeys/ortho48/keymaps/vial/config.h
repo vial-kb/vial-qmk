3ebafadaf104e0376e14528e5cecc957e1810be7
victorlucachi <lucachi.victor@gmail.com>
2023-01-12 10:15:20 +0200

    added ortho48 vial keymap

---
+++ keyboards/cannonkeys/ortho48/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x10, 0x7E, 0x4B, 0x38, 0x13, 0xDB, 0x37, 0x0C}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+#define VIAL_COMBO_ENTRIES 64
