5057ad826f736359f1c08ad36c9f70292bdd44b8
pies-n-loaf <amwright.dev@gmail.com>
2023-01-13 23:31:38 -0600

    Change the unlock combo to esc + tilde

--- keyboards/cannonkeys/rekt1800/keymaps/vial/config.h
+++ keyboards/cannonkeys/rekt1800/keymaps/vial/config.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x61, 0x77, 0x27, 0x2A, 0x75, 0x02, 0x58, 0xC5}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
 
 #define BACKLIGHT_BREATHING
