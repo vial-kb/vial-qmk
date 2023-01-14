b7408cc283e1bf93973b1171d98c248e9c383167
pies-n-loaf <amwright.dev@gmail.com>
2023-01-13 22:27:55 -0600

    Add initial files for rekt1800 vial config

---
+++ keyboards/cannonkeys/rekt1800/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x61, 0x77, 0x27, 0x2A, 0x75, 0x02, 0x58, 0xC5}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
---
+++ keyboards/cannonkeys/rekt1800/keymaps/vial/rules.mk
@@ -0,0 +1,6 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# Options to reduce firmware size:
+LTO_ENABLE = yes  # make the compiler work harder
+QMK_SETTINGS = no # enables the tab to change QMK settings from the GUI
