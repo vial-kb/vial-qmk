4525050dbaff984068352723c65d61a608f08632
adophoxia <100170946+adophoxia@users.noreply.github.com>
2023-01-14 19:34:23 -0800

    Port Keychron Q7 to VIAL (#330)
    * Port Keychron Q6 to VIAL
    
    * Update VIAL UIDs for ISO variants of Q6

---
+++ keyboards/keychron/q6/ansi/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9C, 0x64, 0xE8, 0x7E, 0x6E, 0xB4, 0x96, 0x74}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
---
+++ keyboards/keychron/q6/ansi_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x99, 0xB7, 0xD1, 0xF5, 0x63, 0x7B, 0xA9, 0x04}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
