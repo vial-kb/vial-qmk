a6554d242f5919e2fdfa37135ea1103528f238fc
adophoxia <100170946+adophoxia@users.noreply.github.com>
2023-01-14 19:34:50 -0800

    Port VIAL to Keychron Q10 (#333)

---
+++ keyboards/keychron/q10/iso_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE6, 0x2F, 0xF7, 0x8E, 0x5D, 0x94, 0x53, 0x44}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
