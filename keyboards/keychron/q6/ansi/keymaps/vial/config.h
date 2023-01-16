f50cd7fb616222b043d15e9bea95dee094a8ad32
Adophoxia <andyao1528@gmail.com>
2023-01-15 19:51:45 -0800

    Switch `VIAL_UNLOCK_COMBO_ROWS` for Q3, Q5, Q6, Q9 and Q10 and update JSONs for Q8, and Q9

--- keyboards/keychron/q6/ansi/keymaps/vial/config.h
+++ keyboards/keychron/q6/ansi/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0x9C, 0x64, 0xE8, 0x7E, 0x6E, 0xB4, 0x96, 0x74}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
