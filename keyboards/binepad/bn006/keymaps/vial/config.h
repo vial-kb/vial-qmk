f38bca48c7387cc312009ee52b7ebe1025a74ffd
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-02-07 14:02:26 +1100

    Add Binepad BN006 6-key macro pad

---
+++ keyboards/binepad/bn006/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* Copyright 2022 Binepad (@binpad) */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE8, 0xA8, 0x51, 0x14, 0xBC, 0x64, 0xFB, 0x65}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
