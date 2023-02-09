b31713c80bee7f112df6b163326ab1289f7fa7ed
Erik Trinkle <ectrink@gmail.com>
2023-02-08 19:43:01 -0500

    fix copyright

--- keyboards/cyboard/dactyl/keymaps/dualarcs_function_row/keymap.c
+++ keyboards/cyboard/dactyl/keymaps/dualarcs_function_row/keymap.c
@@ -1,12 +1,8 @@
-// Copyright 2021 cyboard (@cyboard)
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright 2023 Cyboard LLC (@Cyboard-DigitalTailor)
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 
 #include QMK_KEYBOARD_H
-
-// Defines names for use in layer keycodes and the keymap
-//enum layer_names {
-//    BASE,  // default layer
-//};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
