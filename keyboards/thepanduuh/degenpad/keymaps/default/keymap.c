7c88e6d88a484aa41b756b73ce09f751d1b231df
ThePanduuh <feojoe2@gmail.com>
2023-02-12 14:34:38 -0500

    Add default keymap, update parameters to match QMK

--- keyboards/thepanduuh/degenpad/keymaps/default/keymap.c
+++ keyboards/thepanduuh/degenpad/keymaps/default/keymap.c
@@ -16,9 +16,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
+// Defines names for use in layer keycodes and the keymap
+
+enum layer_names {
+	_BASE,
+	_FN
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /*Base*/
-  [0] = LAYOUT_ortho_6x4(
+
+  [_BASE] = LAYOUT_ortho_6x4(
   KC_ESC , KC_EQL , KC_BSPC, MO(1)  ,
   KC_NUM , KC_PSLS, KC_PAST, KC_PMNS,
   KC_P7  , KC_P8  , KC_P9  , KC_PPLS,
@@ -27,37 +34,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_P0  , KC_P0  , KC_PDOT, KC_PENT
   ),
 
-  [1] = LAYOUT_ortho_6x4(
+  [_FN] = LAYOUT_ortho_6x4(
   KC_TRNS, KC_TRNS, KC_DEL , KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_HOME, KC_UP  , KC_PGUP, KC_TRNS,
   KC_LEFT, KC_TRNS, KC_RGHT, KC_TRNS,
   KC_END , KC_DOWN, KC_PGDN, QK_BOOT,
   KC_INS , KC_INS , KC_DEL , QK_BOOT
-  ),
-  [2] = LAYOUT_ortho_6x4(
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-  [3] = LAYOUT_ortho_6x4(
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
+  )
+
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-  [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-  [1] = {ENCODER_CCW_CW(KC_NO, KC_NO)},
-  [2] = {ENCODER_CCW_CW(KC_NO, KC_NO)},
-  [3] = {ENCODER_CCW_CW(KC_NO, KC_NO)},
+  [_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+  [_FN]   = {ENCODER_CCW_CW(KC_NO, KC_NO)},
 };
 #endif
