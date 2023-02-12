4bd8ad9f7d9434538ff25a57020d7651a178d6d3
Erik Trinkle <ectrink@gmail.com>
2023-02-12 08:16:17 -0500

    try a default-named config

---
+++ keyboards/cyboard/dactyl/keymaps/default/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC4, 0xCC, 0xB4, 0x76, 0x30, 0x16, 0xFC, 0x70}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 10, 10 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 6 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
