f119eeee952cd5544702e7286c5b6eaaa2e37bb5
Ben Roe <ben@MacBook-Pro-von-Ben.local>
2023-02-15 12:25:40 +0100

    Fix: add missing split pointing device setting

--- keyboards/keycapsss/3w6_2040/config.h
+++ keyboards/keycapsss/3w6_2040/config.h
@@ -11,9 +11,11 @@
 #define SERIAL_USART_SPEED 921600 // Sped :)
 #define USB_VBUS_PIN GP19         // for rp2040 | When USB_VBUS_PIN is not defined, SPLIT_USB_DETECT is used.
 #define EE_HANDS                  // Split handedness via eeprom
-#define SPLIT_LED_STATE_ENABLE    // sync host leds (caps lock, ...)
-#define SPLIT_LAYER_STATE_ENABLE
-#define SPLIT_POINTING_ENABLE
+#define SPLIT_LED_STATE_ENABLE    // Sync host leds (caps lock, ...)
+#define SPLIT_LAYER_STATE_ENABLE  // Enables syncing of the layer state between both halves
+#define SPLIT_POINTING_ENABLE     // Transmitting the pointing device status to the master side
+#define POINTING_DEVICE_RIGHT     // Pointing device on the right side (Required - pick one only)
+//#define POINTING_DEVICE_LEFT    // Pointing device on the left side (Required - pick one only)
 
 #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_EFFECT_BREATHING + 2
 #define RGBLIGHT_DEFAULT_HUE 152
