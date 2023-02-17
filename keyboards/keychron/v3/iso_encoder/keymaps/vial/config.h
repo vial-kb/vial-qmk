6c002a618bd8b4790138b3129a0bde6861c61808
adophoxia <100170946+adophoxia@users.noreply.github.com>
2023-02-16 23:22:58 -0800

    Port Keychron V3 to VIAL (#369)
    * Port Keychron V3 to VIAL
    
    * Update vial.json for Keychron V3 `jis_encoder` variant
    
    * Change 2nd for unlock combo to correct index for the ANSI variants of the V3
    
    * Change `rgb_matrix_indicators_advanced_kb` function to void
    
    * Revert references of `RGB_MATRIX_LED_COUNT` to `DRIVER_LED_TOTAL`

---
+++ keyboards/keychron/v3/iso_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x16, 0x11, 0x43, 0x4D, 0xED, 0x52, 0x88, 0x36}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
