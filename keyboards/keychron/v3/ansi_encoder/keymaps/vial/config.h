22a5c7d876add7e889021e596c9c928514320301
Adophoxia <andyao1528@gmail.com>
2023-02-16 19:28:42 -0800

    Change 2nd for unlock combo to correct index for the ANSI variants of the V3

--- keyboards/keychron/v3/ansi_encoder/keymaps/vial/config.h
+++ keyboards/keychron/v3/ansi_encoder/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0x3E, 0xD9, 0x53, 0x3C, 0x7F, 0xCC, 0x10, 0x02}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
