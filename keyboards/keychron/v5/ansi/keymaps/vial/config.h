b204e8a01f4e04c2d078306da69301105d88f6ae
Adophoxia <andyao1528@gmail.com>
2023-02-16 19:42:32 -0800

    Change 2nd for unlock combo to correct index for the ANSI and ISO variants of the V5

--- keyboards/keychron/v5/ansi/keymaps/vial/config.h
+++ keyboards/keychron/v5/ansi/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0x71, 0x20, 0x1E, 0x00, 0x51, 0xB1, 0xCB, 0xCF}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
