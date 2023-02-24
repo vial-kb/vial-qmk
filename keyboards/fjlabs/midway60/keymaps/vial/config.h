c508379407dbdf4f8085cb40d324581fc31d5614
Gabriel Freire <gabrielfvale@gmail.com>
2023-02-24 13:01:13 -0300

    Port Midway60 to VIAL

---
+++ keyboards/fjlabs/midway60/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD7, 0x8E, 0x9E, 0x0E, 0xD6, 0xC2, 0xD4, 0x18}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
