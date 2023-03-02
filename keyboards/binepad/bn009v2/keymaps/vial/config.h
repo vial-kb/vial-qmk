4c9c42fda5e880ae3796515f8c4cd56b3b855349
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-03-02 23:50:16 +1100

    Add Binepad BN009 v2

---
+++ keyboards/binepad/bn009v2/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Binepad
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x48, 0x7D, 0x01, 0xAC, 0xB2, 0x33, 0x46, 0x84}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
