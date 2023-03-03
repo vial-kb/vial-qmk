aeddefedf665bf0132ac644e75a475bbc53e4ce8
stanrc85 <stanrc@gmail.com>
2023-03-03 05:30:47 -0500

    Remove personal code

--- keyboards/vertex/arc60h/keymaps/vial/keymap.c
+++ keyboards/vertex/arc60h/keymaps/vial/keymap.c
@@ -49,28 +49,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,          _______,                   _______,                                     _______, _______,          _______
     ),
 };
-
-void keyboard_post_init_user(void) {
-  rgblight_setrgb(0x00, 0x00, 0x00);
-};
-/*
-layer_state_t layer_state_set_user(layer_state_t state) {
-  switch (get_highest_layer(state)) {
-    case 0:
-      rgblight_setrgb (0x00, 0x00, 0x00);
-      break;
-    case 1:
-      rgblight_setrgb (0x00, 0xFF, 0x00);
-      break;
-    case 2:
-      rgblight_setrgb (0x00, 0x00, 0xFF);
-      break;
-    case 3:
-      rgblight_setrgb (0xFF, 0x00, 0x00);
-      break;
-    default: //  for any other layers, or the default layer
-      rgblight_setrgb (0x00, 0x00, 0x00);
-      break;
-    }
-  return state;
-}*/
