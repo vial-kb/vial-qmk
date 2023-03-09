dd5c7ec956a63c35e5c82088eae02efbe7a6e2b2
adophoxia <100170946+adophoxia@users.noreply.github.com>
2023-03-08 20:37:00 -0800

    Port Keychron Q12 to VIAL (#401)
    * Port Keychron Q12 to VIAL
    
    * Update VIAL UIDs for Keychron Q12

---
+++ keyboards/keychron/q12/iso_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5A, 0x1E, 0xFA, 0xD1, 0x03, 0x0B, 0x4E, 0xF5}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
