ce22824b92d45f55a960b0c0e25bdded381202e4
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2023-03-20 23:07:16 -0400

    Update keyboards/mini49/keymaps/vial/keymap.c
    rename `LAYOUT_mini49` to `LAYOUT`
    
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/mini49/keymaps/vial/keymap.c
+++ keyboards/mini49/keymaps/vial/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT_mini49(
+    [0] = LAYOUT(
         KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSPC,
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
