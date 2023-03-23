6117821f559b37475899b1bb35dab807a3acf40e
Andrew Litt <al177@users.noreply.github.com>
2023-03-22 23:20:05 -0500

    Add Koolertron AMAG09 9 key macropad

---
+++ keyboards/koolertron/amag09/rules.mk
@@ -0,0 +1,15 @@
+# MCU name
+MCU = atmega32a
+
+# Bootloader selection
+BOOTLOADER = bootloadhid
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
