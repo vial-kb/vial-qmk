69b81ee3b4fb20ee8937f1f59ff2fa4b0ccdd135
Patrick Muldoon <doon@labratsoftware.com>
2023-03-29 21:40:55 -0700

    Adding Mode Sonnet, both HotSwap and Solder PCB.
    Following the porting guide this adds The Mode Sonnet, both hotswap and solder variants.
    
    Tested on multiple keyboards.

---
+++ keyboards/mode/m75s/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3D, 0x6C, 0x63, 0x19, 0xA7, 0x1F, 0x0E, 0x59}
+
+/* Unlock with esc / del */
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
