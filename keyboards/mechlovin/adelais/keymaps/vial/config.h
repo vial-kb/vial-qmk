a250b282bcb1195ef9a0d2621ec324a810da8175
dpoplin <dpoplin@paypal.com>
2023-04-19 17:07:05 -0500

    add mechlovin adelais

---
+++ keyboards/mechlovin/adelais/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x98, 0xF1, 0x64, 0xD6, 0xB9, 0xC7, 0x3E, 0xF2}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
