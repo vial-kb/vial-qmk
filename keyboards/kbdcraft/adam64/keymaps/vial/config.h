cd34f82c500a2eb00395a994b85ca97cbcc0c5e3
Adophoxia <andyao1528@gmail.com>
2023-04-19 20:29:25 -0700

    Add `KBDCraft Adam64` to VIAL

---
+++ keyboards/kbdcraft/adam64/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFD, 0xD6, 0x04, 0x27, 0x11, 0x37, 0xB0, 0xB0}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
