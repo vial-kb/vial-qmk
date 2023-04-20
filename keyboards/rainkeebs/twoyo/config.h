6d43b534e750084d919420f3e25796339675d9c9
dpoplin <dpoplin@paypal.com>
2023-04-20 14:32:42 -0500

    add config.h

--- keyboards/rainkeebs/twoyo/config.h
+++ keyboards/rainkeebs/twoyo/config.h
@@ -33,7 +33,6 @@
 #define MATRIX_COL_PINS { D1, B6, B2, B3, B1, F7 }
 #define MATRIX_ROW_PINS_RIGHT { F4, F7, F6, B6 }
 #define MATRIX_COL_PINS_RIGHT { D4, C6, D7, E6, B4, B5 }
-#define UNUSED_PINS
 
 
 /* encoder pins */
