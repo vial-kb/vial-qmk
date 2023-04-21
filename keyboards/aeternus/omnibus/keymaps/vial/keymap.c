3578d6e2a074650bf78b3f340b92d0d2dc77508c
dpoplin <dpoplin@paypal.com>
2023-04-21 15:37:56 -0500

    add encoder functionality

--- keyboards/aeternus/omnibus/keymaps/vial/keymap.c
+++ keyboards/aeternus/omnibus/keymaps/vial/keymap.c
@@ -15,6 +15,15 @@
  */
 #include QMK_KEYBOARD_H
 
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
+    [1] =   { ENCODER_CCW_CW(RGB_HUD, RGB_HUI) },
+    [2] =   { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [3] =   { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD) },
+};
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [0] = LAYOUT_all(
