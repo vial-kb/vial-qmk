7d9060f7e47a7d86feb6b79c6bb895aab6716c55
Dong Zhou <zhou.dong@gmail.com>
2023-04-30 14:10:33 -0400

    clean up rules.mk

--- keyboards/splitkb/kyria/rev2/keymaps/vial/rules.mk
+++ keyboards/splitkb/kyria/rev2/keymaps/vial/rules.mk
@@ -8,22 +8,15 @@ BOOTLOADER = caterina
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no # Mouse keys
+MOUSEKEY_ENABLE = yes 		# Mouse keys
 EXTRAKEY_ENABLE = yes      # Audio control and System control
-CONSOLE_ENABLE = no        # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no          # Audio output
-SPLIT_KEYBOARD = yes       # Split common
+
+
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
-
-DEFAULT_FOLDER = splitkb/kyria/rev2
-
+OLED_ENABLE = no
 
 AUTO_SHIFT_ENABLE = yes
 COMBO_ENABLE = yes
@@ -32,7 +25,6 @@ TAP_DANCE_ENABLE = no
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
-LTO_ENABLE = yes
 QMK_SETTINGS = yes
 KEY_OVERRIDE_ENABLE = no
 DEFERRED_EXEC_ENABLE = no
