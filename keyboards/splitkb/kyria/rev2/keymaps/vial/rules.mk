ab3356d812cf8a8eefcca49acac041ab27f406bf
Dong Zhou <zhou.dong@gmail.com>
2023-05-06 12:23:57 -0400

    more clean up

--- keyboards/splitkb/kyria/rev2/keymaps/vial/rules.mk
+++ keyboards/splitkb/kyria/rev2/keymaps/vial/rules.mk
@@ -1,20 +1,9 @@
-# MCU name
-MCU = atmega32u4
-
 # Bootloader selection
-BOOTLOADER = caterina
+# BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes 		# Mouse keys
-EXTRAKEY_ENABLE = yes      # Audio control and System control
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-
-
-ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 OLED_ENABLE = no
 
@@ -27,12 +16,7 @@ VIAL_ENABLE = yes
 
 QMK_SETTINGS = yes
 KEY_OVERRIDE_ENABLE = no
-DEFERRED_EXEC_ENABLE = no
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no
 AVR_USE_MINIMAL_PRINTF = yes
-DYNAMIC_MACRO_ENABLE = no
-MUSIC_ENABLE = no
-SWAP_HANDS_ENABLE= no
-
