b383b554181424ff68a4df5224ec6f8708ec529e
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2023-05-09 20:34:02 +0800

    Add vial keymap for RS60

---
+++ keyboards/xelus/rs60/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB5, 0xCF, 0x0C, 0xD3, 0x06, 0xBA, 0xDB, 0x39}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
