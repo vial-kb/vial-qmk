2d7c2b1f68feb73edd505675d69381e25c9ae8b2
David Gillies <dave.gillies@gmail.com>
2023-05-08 22:14:43 -0700

    Add Drop ALT support

---
+++ keyboards/massdrop/alt/keymaps/vial/config.h
@@ -0,0 +1,26 @@
+/* Copyright 2022 SlyCedix (slycedix@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// Enable additional LED functionality
+#define USB_LED_INDICATOR_ENABLE
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define VIAL_KEYBOARD_UID {0x98, 0x35, 0xCE, 0x25, 0x67, 0xB8, 0x2E, 0x49}
---
+++ keyboards/massdrop/alt/keymaps/vial/rules.mk
@@ -0,0 +1,7 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# Uncomment the following to enable specific features (Increases firmware size)
+# MOUSEKEY_ENABLE = yes			# Allow the use of keys
+# CONSOLE_ENABLE = yes         	# Console for debug
+# COMMAND_ENABLE = yes         	# Commands for debug and configuration
