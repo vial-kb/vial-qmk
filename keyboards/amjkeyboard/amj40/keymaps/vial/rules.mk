31d38b068be42212ae2dda1d1b680324076216f2
居戎氏 <chen.sst@gmail.com>
2023-05-11 17:25:56 +0800

    Port amj40 to Vial

---
+++ keyboards/amjkeyboard/amj40/keymaps/vial/rules.mk
@@ -0,0 +1,11 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+# disable unused QMK features to reduce firmware size
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+# disable unused Vial features to reduce firmware size
+QMK_SETTINGS = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
