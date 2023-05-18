dcc0b8d71f8bef7fbd73d7364e1ef12442b28924
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-05-18 10:35:03 +1000

    Add Moondrop Dash75

---
+++ keyboards/moondrop/dash75/r1/config.h
@@ -0,0 +1,8 @@
+// Copyright 2023 moondroplab (@moondroplab)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define USB_SUSPEND_WAKEUP_DELAY 5000  // wait for USB hub to start
+
+#define HOLD_ON_OTHER_KEY_PRESS
