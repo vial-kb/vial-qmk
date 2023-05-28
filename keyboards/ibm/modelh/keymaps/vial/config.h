105915582a4cfc15a9422f9633eccc656dd1cbb2
Jerome Berclaz <jerome@percipient.ai>
2023-05-28 02:14:59 -0700

    New keyboard: Model H controller for IBM Model M (#469)
    * added modelh controller
    
    * updated readme

---
+++ keyboards/ibm/modelh/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBC, 0x8E, 0x44, 0xF1, 0x5C, 0x16, 0xD6, 0x38}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 6, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 6, 15 }
