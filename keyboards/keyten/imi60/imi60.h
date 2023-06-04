24d5e7edd3a4cf06a4f1b27321a0524ec59cbc6e
Ivan Gromov <38141348+key10iq@users.noreply.github.com>
2023-06-05 00:08:51 +0400

    Create imi60.h

---
+++ keyboards/keyten/imi60/imi60.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#include "quantum.h"
+
+#define XXX KC_NO
+
+#define LAYOUT( \
+		k0A, k0B, k0C, k0D, k0E, k0F, k0G, k0H, k0I, k0J, k0K, k0L, k0M, k0N, k2N, \
+		k1A, k1B, k1C, k1D, k1E, k1F, k1G, k1H, k1I, k1J, k1K, k1L, k1M, k1N, \
+		k2A, k2B, k2C, k2D, k2E, k2F, k2G, k2H, k2I, k2J, k2K, k2L, k2M, \
+		k3A, k3B, k3C, k3D, k3E, k3F, k3G, k3H, k3I, k3J, k3K, k3L, k3M, k3N, \
+		k4A, k4B, k4D, k4H, k4L, k4M, k4N \
+) \
+{ \
+	{k0A, k0B, k0C, k0D, k0E, k0F, k0G, k0H, k0I, k0J, k0K, k0L, k0M, k0N}, \
+	{k1A, k1B, k1C, k1D, k1E, k1F, k1G, k1H, k1I, k1J, k1K, k1L, k1M, k1N}, \
+	{k2A, k2B, k2C, k2D, k2E, k2F, k2G, k2H, k2I, k2J, k2K, k2L, k2M, k2N}, \
+	{k3A, k3B, k3C, k3D, k3E, k3F, k3G, k3H, k3I, k3J, k3K, k3L, k3M, k3N}, \
+	{k4A, k4B, XXX, k4D, XXX, XXX, XXX, k4H, XXX, XXX, XXX, k4L, k4M, k4N} \
+}
