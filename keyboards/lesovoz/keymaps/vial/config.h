74015d41e5a4f447abaab55f93fb58f1cc29fa34
D0hnut5 <wmmillar@gmail.com>
2023-07-19 16:20:12 -0500

    Added Lesovoz vial
    Increased combos limit to 12
    Updated keymap
    Updated bootloader
    Created info.json

---
+++ keyboards/lesovoz/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA8, 0x18, 0x34, 0xBA, 0x42, 0xB6, 0x01, 0x9B}
+
+#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 1
+
+
+#define VIAL_COMBO_ENTRIES 12
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
---
+++ keyboards/lesovoz/keymaps/vial/rules.mk
@@ -0,0 +1,6 @@
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+VIAL_INSECURE = yes
