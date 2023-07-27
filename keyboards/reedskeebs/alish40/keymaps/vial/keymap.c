9fb368dee2297f931dd5182da5af0dbffcdd3045
honorless <86894501+lesshonor@users.noreply.github.com>
2023-07-27 18:14:39 -0400

    fix: vial keymap

--- keyboards/reedskeebs/alish40/keymaps/vial/keymap.c
+++ keyboards/reedskeebs/alish40/keymaps/vial/keymap.c
@@ -1,30 +1,28 @@
-#include "kb.h"
+// Copyright 2023 Vial Contributors (@vial-qmk)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-	KEYMAP(
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
-		MO(2), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_ENT, 
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_LSFT, 
-		KC_LCTL, TG(3), KC_LALT, MO(1), KC_SPC, KC_LCTL, KC_LEFT, KC_RGHT),
-
-	KEYMAP(
-		KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-
-	KEYMAP(
-		KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_VOLD, KC_VOLU, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-
-	KEYMAP(
-		KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-
+    /* Base */
+    [0] = LAYOUT_all(
+        KC_ESC,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
+        MO(1),             KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_QUOT,  KC_ENT,
+        KC_LSFT,  _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+        KC_LCTL,  KC_LGUI, KC_MUTE,                   KC_SPC,                    KC_SPC,            KC_RALT,  KC_RGUI,  KC_RCTL
+    ),
+    [1] = LAYOUT_all(
+        _______,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,    KC_DEL,
+        _______,            _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, KC_SCLN,
+        _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______,
+        _______,  _______,  _______,                   _______,                   _______,           _______,  _______, _______
+        )
 };
 
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] =  { ENCODER_CCW_CW(RGB_SAD, RGB_SAI)  }
+};
+#endif
--- keyboards/reedskeebs/alish40/keymaps/vial/rules.mk
+++ keyboards/reedskeebs/alish40/keymaps/vial/rules.mk
@@ -1,5 +1,11 @@
-# Enable VIAL Support 
-VIAL_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
 VIA_ENABLE = yes
-VIAL_ENCODERS_ENABLE = yes
-LTO_ENABLE = yes
+VIAL_ENABLE = yes
+
+ifeq ($(strip $(MCU)), atmega32u4)
+    GRAVE_ESC_ENABLE = no
+    KEY_OVERRIDE_ENABLE = no
+    LTO_ENABLE = yes
+    MAGIC_ENABLE = no
+    SPACE_CADET_ENABLE = no
+endif
