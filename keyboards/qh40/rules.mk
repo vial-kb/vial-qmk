b135d7a9834122f81485555cb90d67279f3b3884
shenguoquan <shenguoquan@meituan.com>
2023-08-08 18:15:09 +0800

    qh95

--- keyboards/qh40/rules.mk
+++ keyboards/qh40/rules.mk
@@ -1,24 +1,30 @@
-# MCU name
-# MCU = atmega32u4
-
-# Bootloader selection
-# BOOTLOADER = caterina
-
 # Build Options
-# BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-# MOUSEKEY_ENABLE = yes       # Mouse keys
-# EXTRAKEY_ENABLE = yes       # Audio control and System control
-# COMBO_ENABLE = yes
-# CONSOLE_ENABLE = no         # Console for debug
-# COMMAND_ENABLE = no         # Commands for debug and configuration
-# NKRO_ENABLE = yes           # Enable N-Key Rollover
-# AUDIO_ENABLE = no           # Audio output
-# LTO_ENABLE = yes
-# SPLIT_KEYBOARD = yes        # Enables split keyboard support
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+COMBO_ENABLE = yes
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+# Additional thing to reduce compiled size
+LTO_ENABLE = yes
 # KEY_LOCK_ENABLE = yes
+KEY_LOCK_ENABLE = yes
 
-# rgb matrix
-# RGB_MATRIX_ENABLE = yes
-# RGB_MATRIX_DRIVER = WS2812
-# BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-# RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+AUDIO_ENABLE = no           # Audio output
+KEY_OVERRIDE_ENABLE = no
+# SEND_STRING_ENABLE = no
+SECURE_ENABLE = no
+SEQUENCER_ENABLE = no
+AUTO_SHIFT_ENABLE = no
+CAPS_WORD_ENABLE = no
+
+SPLIT_KEYBOARD = yes        # Enables split keyboard support
+
+# media
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+# RGB Matrix enabled
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+
--- keyboards/qh40/v4/rules.mk
+++ keyboards/qh40/v4/rules.mk
@@ -6,41 +6,8 @@ BOOTLOADER = uf2boot
 # BOOTLOADER = stm32duino
 BOARD = STM32_F103_STM32DUINO
 
-# Build Options
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-COMBO_ENABLE = yes
+
 TAP_DANCE_ENABLE = yes
-KEY_LOCK_ENABLE = no
-KEY_OVERRIDE_ENABLE = no
-# SEND_STRING_ENABLE = no
-SECURE_ENABLE = no
-SEQUENCER_ENABLE = no
-AUTO_SHIFT_ENABLE = no
-CAPS_WORD_ENABLE = no
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-
-# KEY_LOCK_ENABLE = yes
-SPLIT_KEYBOARD = yes        # Enables split keyboard support
-
-
-# media
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-# RGB Matrix enabled
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
 WS2812_DRIVER = pwm
-
-# Additional thing to reduce compiled size
-LTO_ENABLE = yes
-
 SERIAL_DRIVER = usart
 
-
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-# OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
