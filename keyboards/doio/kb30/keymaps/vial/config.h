50ba7be1f7e322578a7a42b3865e44accc1a0d5a
crkochan <5183896+crkochan@users.noreply.github.com>
2023-08-24 19:05:41 -0700

    Support for DOIO KB30-01

---
+++ keyboards/doio/kb30/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC3, 0xD7, 0xE9, 0x8D, 0x71, 0xBB, 0xA3, 0xEC}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
+
+#define ENCODER_MAP_ENABLE
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
