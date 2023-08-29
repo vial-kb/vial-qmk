ae6d105edf94fe16c2d86aa962d6af77e7ad4b0d
honorless <86894501+lesshonor@users.noreply.github.com>
2023-08-29 08:07:57 -0400

    refactor: improve code for QMK merge

--- keyboards/ymdk/ymd09/keymaps/vial/keymap.c
+++ keyboards/ymdk/ymd09/keymaps/vial/keymap.c
@@ -8,13 +8,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT(RGB_RMOD, RGB_VAI, RGB_MOD,
                RGB_HUI, QK_BOOT, RGB_SAI,
-               RGB_HUD, RGB_VAD, RGB_SAD),
-
-  [2] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS),
-
-  [3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS),
+               RGB_HUD, RGB_VAD, RGB_SAD)
 };
