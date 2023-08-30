1923cc35cae940254678fd24d6243a05ba22dbb8
NCKiser <noahkiser95mygc@gmail.com>
2023-08-29 19:32:29 -0500

    Match number of layers b/t keymap and encodermap
    This reverts commit db72bf0baf69044d8ca9447170b051fb0daab2cb.

--- keyboards/kiserdesigns/madeline/keymaps/vial/keymap.c
+++ keyboards/kiserdesigns/madeline/keymaps/vial/keymap.c
@@ -41,7 +41,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-    [1] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [2] =   { ENCODER_CCW_CW(KC_NO, KC_NO) }
+    [1] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif
