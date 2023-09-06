2db3155fb753f6411e5c92353c2416bbc53bbd0b
mkonji <mkonji.tuta.io>
2023-09-06 15:44:52 -0400

    enable takashicompany_minizone vial

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
@@ -12,16 +11,3 @@ EXTRAKEY_ENABLE     = yes
 COMBO_ENABLE        = no
 
 QMK_SETTINGS        = no
-=======
-MOUSEKEY_ENABLE     = no      # Mouse keys
-RGBLIGHT_ENABLE     = no     # Enable WS2812 RGB underlight.
-VIA_ENABLE          = yes     # Enable VIA
-VIAL_ENABLE         = yes     # Enable VIAL
-OLED_ENABLE         = yes
-OLED_DRIVER         = SSD1306
-LTO_ENABLE          = yes
-QMK_SETTINGS        = no
-COMBO_ENABLE        = yes
-KEY_OVERRIDE_ENABLE = no
-TAP_DANCE_ENABLE    = no
->>>>>>> 98aa80e333 (increased feature defaults for crkbd vial and added vial support for takashicompany/minizone)
