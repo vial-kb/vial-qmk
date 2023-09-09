f1f441aa32bf8805bed8a8173b7050a2f49aa102
honorless <86894501+lesshonor@users.noreply.github.com>
2023-09-09 01:38:34 -0400

    fix: broken/outdated firmware

--- keyboards/gizmo_engineering/gk6/keymaps/vial/rules.mk
+++ keyboards/gizmo_engineering/gk6/keymaps/vial/rules.mk
@@ -1,12 +1,3 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = atmel-dfu
-
-# Build Options
-#   change yes to no to disable
-
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
@@ -16,17 +7,3 @@ QMK_SETTINGS = no
 TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
 KEY_OVERRIDE_ENABLE = no
-
-#
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = IS31FL3731
