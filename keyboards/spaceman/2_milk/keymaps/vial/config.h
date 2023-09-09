b04652cd79cac53322e3c9f4e496bacd4facf192
Lenbok <lenbok@gmail.com>
2023-09-10 09:08:52 +1200

    Port support for spaceman 2% milk macropad

---
+++ keyboards/spaceman/2_milk/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x50, 0xB8, 0x6D, 0x63, 0x19, 0xE1, 0xC6, 0xEC}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
