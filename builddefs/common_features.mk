8d8a0cdeccb92976969f4959ac56d3a5d28f05f7
honorless <86894501+lesshonor@users.noreply.github.com>
2023-09-09 12:05:30 -0400

    refactor: create Vial-specific makefile
    Isolating Vial build rules and recipes makes it clearer where and why
    the codebase diverges and prevents unintentional modifications after
    upstream changes.

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -627,38 +627,7 @@ ifeq ($(strip $(VIA_ENABLE)), yes)
 endif
 
 ifeq ($(strip $(VIAL_ENABLE)), yes)
-    QMK_SETTINGS ?= yes
-    TAP_DANCE_ENABLE ?= yes
-    ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
-        OPT_DEFS += -DTAPPING_TERM_PER_KEY
-    endif
-    COMBO_ENABLE ?= yes
-    KEY_OVERRIDE_ENABLE ?= yes
-    SRC += $(QUANTUM_DIR)/vial.c
-    OPT_DEFS += -DVIAL_ENABLE -DNO_DEBUG -DSERIAL_NUMBER=\"vial:f64c2b3c\"
-
-$(QUANTUM_DIR)/vial.c: $(INTERMEDIATE_OUTPUT)/src/vial_generated_keyboard_definition.h
-
-$(INTERMEDIATE_OUTPUT)/src/vial_generated_keyboard_definition.h: $(KEYMAP_PATH)/vial.json
-	python3 util/vial_generate_definition.py $(KEYMAP_PATH)/vial.json $(INTERMEDIATE_OUTPUT)/src/vial_generated_keyboard_definition.h
-endif
-
-ifeq ($(strip $(VIAL_INSECURE)), yes)
-    OPT_DEFS += -DVIAL_INSECURE
-endif
-
-ifeq ($(strip $(VIALRGB_ENABLE)), yes)
-    SRC += $(QUANTUM_DIR)/vialrgb.c
-    OPT_DEFS += -DVIALRGB_ENABLE
-endif
-
-ifeq ($(strip $(QMK_SETTINGS)), yes)
-    AUTO_SHIFT_ENABLE := yes
-    SRC += $(QUANTUM_DIR)/qmk_settings.c
-    OPT_DEFS += -DQMK_SETTINGS \
-        -DAUTO_SHIFT_NO_SETUP -DAUTO_SHIFT_REPEAT_PER_KEY -DAUTO_SHIFT_NO_AUTO_REPEAT_PER_KEY \
-        -DPERMISSIVE_HOLD_PER_KEY -DHOLD_ON_OTHER_KEY_PRESS_PER_KEY -DTAPPING_FORCE_HOLD_PER_KEY -DRETRO_TAPPING_PER_KEY \
-        -DCOMBO_TERM_PER_COMBO
+    include $(BUILDDEFS_PATH)/build_vial.mk
 endif
 
 VALID_MAGIC_TYPES := yes
