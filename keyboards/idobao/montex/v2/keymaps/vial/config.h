b7e0d6f6b2fa86e5d60db843dd4f85c5303ee910
OTLabs <oleg.titov@gmail.com>
2023-09-17 22:58:35 -0600

    Add Vial support for idobao/montex/v2

---
+++ keyboards/idobao/montex/v2/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Oleg Titov <oleg.titov@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1A, 0xC2, 0xE7, 0x4C, 0xE4, 0x37, 0xF3, 0x6A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
---
+++ keyboards/idobao/montex/v2/keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE =  yes  # Enable VIA
+VIAL_ENABLE = yes  # Enable VIAL
+
+# Options to reduce firmware size:
+LTO_ENABLE   = yes # make the compiler work harder
+QMK_SETTINGS = no # enables the tab to change QMK settings from the GUI1~# enables the tab to change QMK settings from the GUI1~ # enables the tab to change QMK settings from the GUI
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = no
