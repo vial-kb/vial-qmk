e7d7f129ffb56610133817a30ca980bf619e35b1
TweetyDaBird <markus.knutsson@tweety.se>
2023-09-27 14:01:41 +0200

    adding vial id

---
+++ keyboards/tweetydabird/lotus58/keymaps/vial/rules.mk
@@ -0,0 +1,13 @@
+# Copyright 2022 Markus Knutsson (@TweetyDaBird)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Needed for VIAL compatability (all are needed)
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+# Reduce size by disabling
+TAP_DANCE_ENABLE = no
+QMK_SETTINGS = no
+KEY_OVERRIDE_ENABLE = no
+COMBO_ENABLE = no
