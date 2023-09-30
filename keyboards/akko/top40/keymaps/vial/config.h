a9f7750316f5e3230c1c3739f7f74b520a0b61ae
Oleg Titov <oleg.titov@gmail.com>
2023-09-30 18:06:24 -0500

    Add vial support for Akko Top40
    Vial port for Akko Top40 keyboard,
    https://en.akkogear.com/product/acr-top-40/

---
+++ keyboards/akko/top40/keymaps/vial/config.h
@@ -0,0 +1,20 @@
+// Copyright 2023 Oleg Titov <oleg.titov@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Unique keyboard id
+#define VIAL_KEYBOARD_UID {0x5D, 0x72, 0x7C, 0xB4, 0xC9, 0x7C, 0xF2, 0x60}
+
+// Unlock with Escape+Enter
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+
+// Enable all available RGB Matrix effects
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+// Firmware size optimization
+// disable Lock Key support
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
