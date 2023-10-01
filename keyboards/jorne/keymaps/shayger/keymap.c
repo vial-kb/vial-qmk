cf67a578d6a785252abe6b9fbaf767c72008d785
shayger <shayger751@gmail.com>
2023-10-01 15:18:40 +0300

    finish working on the shayger layout

--- keyboards/jorne/keymaps/shayger/keymap.c
+++ keyboards/jorne/keymaps/shayger/keymap.c
@@ -183,126 +183,6 @@ static void render_luna(int LUNA_X, int LUNA_Y) {
     }
 }
 
-void render_mod_status_gui_alt(uint8_t modifiers) {
-    static const char PROGMEM gui_off_1[] = {0x85, 0x86, 0};
-    static const char PROGMEM gui_off_2[] = {0xa5, 0xa6, 0};
-    static const char PROGMEM gui_on_1[]  = {0x8d, 0x8e, 0};
-    static const char PROGMEM gui_on_2[]  = {0xad, 0xae, 0};
-    static const char PROGMEM alt_off_1[] = {0x87, 0x88, 0};
-    static const char PROGMEM alt_off_2[] = {0xa7, 0xa8, 0};
-    static const char PROGMEM alt_on_1[]  = {0x8f, 0x90, 0};
-    static const char PROGMEM alt_on_2[]  = {0xaf, 0xb0, 0};
-
-    // fillers between the modifier icons bleed into the icon frames
-    static const char PROGMEM off_off_1[] = {0xc5, 0};
-    static const char PROGMEM off_off_2[] = {0xc6, 0};
-    static const char PROGMEM on_off_1[]  = {0xc7, 0};
-    static const char PROGMEM on_off_2[]  = {0xc8, 0};
-    static const char PROGMEM off_on_1[]  = {0xc9, 0};
-    static const char PROGMEM off_on_2[]  = {0xca, 0};
-    static const char PROGMEM on_on_1[]   = {0xcb, 0};
-    static const char PROGMEM on_on_2[]   = {0xcc, 0};
-
-    if (modifiers & MOD_MASK_GUI) {
-        oled_write_P(gui_on_1, false);
-    } else {
-        oled_write_P(gui_off_1, false);
-    }
-    if ((modifiers & MOD_MASK_GUI) && (modifiers & MOD_MASK_ALT)) {
-        oled_write_P(on_on_1, false);
-    } else if (modifiers & MOD_MASK_GUI) {
-        oled_write_P(on_off_1, false);
-    } else if (modifiers & MOD_MASK_ALT) {
-        oled_write_P(off_on_1, false);
-    } else {
-        oled_write_P(off_off_1, false);
-    }
-    if (modifiers & MOD_MASK_ALT) {
-        oled_write_P(alt_on_1, false);
-    } else {
-        oled_write_P(alt_off_1, false);
-    }
-    if (modifiers & MOD_MASK_GUI) {
-        oled_write_P(gui_on_2, false);
-    } else {
-        oled_write_P(gui_off_2, false);
-    }
-    if (modifiers & MOD_MASK_GUI & MOD_MASK_ALT) {
-        oled_write_P(on_on_2, false);
-    } else if (modifiers & MOD_MASK_GUI) {
-        oled_write_P(on_off_2, false);
-    } else if (modifiers & MOD_MASK_ALT) {
-        oled_write_P(off_on_2, false);
-    } else {
-        oled_write_P(off_off_2, false);
-    }
-    if (modifiers & MOD_MASK_ALT) {
-        oled_write_P(alt_on_2, false);
-    } else {
-        oled_write_P(alt_off_2, false);
-    }
-}
-
-void render_mod_status_ctrl_shift(uint8_t modifiers) {
-    static const char PROGMEM ctrl_off_1[]  = {0x89, 0x8a, 0};
-    static const char PROGMEM ctrl_off_2[]  = {0xa9, 0xaa, 0};
-    static const char PROGMEM ctrl_on_1[]   = {0x91, 0x92, 0};
-    static const char PROGMEM ctrl_on_2[]   = {0xb1, 0xb2, 0};
-    static const char PROGMEM shift_off_1[] = {0x8b, 0x8c, 0};
-    static const char PROGMEM shift_off_2[] = {0xab, 0xac, 0};
-    static const char PROGMEM shift_on_1[]  = {0xcd, 0xce, 0};
-    static const char PROGMEM shift_on_2[]  = {0xcf, 0xd0, 0};
-
-    // fillers between the modifier icons bleed into the icon frames
-    static const char PROGMEM off_off_1[] = {0xc5, 0};
-    static const char PROGMEM off_off_2[] = {0xc6, 0};
-    static const char PROGMEM on_off_1[]  = {0xc7, 0};
-    static const char PROGMEM on_off_2[]  = {0xc8, 0};
-    static const char PROGMEM off_on_1[]  = {0xc9, 0};
-    static const char PROGMEM off_on_2[]  = {0xca, 0};
-    static const char PROGMEM on_on_1[]   = {0xcb, 0};
-    static const char PROGMEM on_on_2[]   = {0xcc, 0};
-
-    if (modifiers & MOD_MASK_CTRL) {
-        oled_write_P(ctrl_on_1, false);
-    } else {
-        oled_write_P(ctrl_off_1, false);
-    }
-    if ((modifiers & MOD_MASK_CTRL) && (modifiers & MOD_MASK_SHIFT)) {
-        oled_write_P(on_on_1, false);
-    } else if (modifiers & MOD_MASK_CTRL) {
-        oled_write_P(on_off_1, false);
-    } else if (modifiers & MOD_MASK_SHIFT) {
-        oled_write_P(off_on_1, false);
-    } else {
-        oled_write_P(off_off_1, false);
-    }
-    if (modifiers & MOD_MASK_SHIFT) {
-        oled_write_P(shift_on_1, false);
-    } else {
-        oled_write_P(shift_off_1, false);
-    }
-    if (modifiers & MOD_MASK_CTRL) {
-        oled_write_P(ctrl_on_2, false);
-    } else {
-        oled_write_P(ctrl_off_2, false);
-    }
-    if (modifiers & MOD_MASK_CTRL & MOD_MASK_SHIFT) {
-        oled_write_P(on_on_2, false);
-    } else if (modifiers & MOD_MASK_CTRL) {
-        oled_write_P(on_off_2, false);
-    } else if (modifiers & MOD_MASK_SHIFT) {
-        oled_write_P(off_on_2, false);
-    } else {
-        oled_write_P(off_off_2, false);
-    }
-    if (modifiers & MOD_MASK_SHIFT) {
-        oled_write_P(shift_on_2, false);
-    } else {
-        oled_write_P(shift_off_2, false);
-    }
-}
-
 void render_layer_state(void) {
     static const char PROGMEM default_layer[] = {0x20, 0x94, 0x95, 0x96, 0x20, 0x20, 0xb4, 0xb5, 0xb6, 0x20, 0x20, 0xd4, 0xd5, 0xd6, 0x20, 0};
     static const char PROGMEM raise_layer[]   = {0x20, 0x97, 0x98, 0x99, 0x20, 0x20, 0xb7, 0xb8, 0xb9, 0x20, 0x20, 0xd7, 0xd8, 0xd9, 0x20, 0};
@@ -325,26 +205,11 @@ bool oled_task_user(void) {
 
     if (is_keyboard_left()) {
         oled_set_cursor(0, 2);
-        if (isSneaking) {
-            render_layer_state();
-        } else {
-            for (size_t i = 0; i < 2; i++) {
-                oled_write("     ", false);
-            }
-        }
         render_luna(0, 6);
     } else {
         oled_set_cursor(0, 6);
         render_layer_state();
         oled_set_cursor(0, 11);
-        if (mod_state) {
-            render_mod_status_gui_alt(get_mods());
-            render_mod_status_ctrl_shift(get_mods());
-        } else {
-            for (size_t i = 0; i < 5; i++) {
-                oled_write("     ", false);
-            }
-        }
     }
     return false;
 }
@@ -382,6 +247,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
             }
             break;
             return false;
+
+        case TAB_RSE:
+
+            if (record->event.pressed) {
+                isSneaking  = true;
+            } else {
+                isSneaking = false;
+            }
+            break;
+            return false;
+
+        case ENT_LWR:
+
+            if (record->event.pressed) {
+                isSneaking  = true;
+            } else {
+                isSneaking = false;
+            }
+            break;
+            return false;
     }
     return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
