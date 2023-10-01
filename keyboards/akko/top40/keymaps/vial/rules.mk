6c482e0e6f17228766efbc4bbdd097cae8a32eab
OTLabs <oleg.titov@gmail.com>
2023-09-30 20:49:49 -0600

    Update keyboards/akko/top40/keymaps/vial/rules.mk
    Remove unnesessary optimization
    
    Co-authored-by: adophoxia <100170946+adophoxia@users.noreply.github.com>

--- keyboards/akko/top40/keymaps/vial/rules.mk
+++ keyboards/akko/top40/keymaps/vial/rules.mk
@@ -1,15 +1,3 @@
 VIA_ENABLE     = yes # Enable VIA
 VIAL_ENABLE    = yes # Enable VIAL
 VIALRGB_ENABLE = yes # Enable VIALRGB
-
-# Options to reduce firmware size:
-LTO_ENABLE = yes # make the compiler work harder
-
-# Vial optimization
-# Enable all as the WB32FQ95 has 256kB flash
-
-# QMK optimization
-# Enable all except mostly never used features
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE   = no
-MAGIC_ENABLE       = no
