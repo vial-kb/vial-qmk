759bbba5811fa96cc4aec2f8fe60e7918e90bbcf
adophoxia <andyao1528@gmail.com>
2023-10-08 14:38:32 -0700

    Add `vial` keymaps for Q1v2

---
+++ keyboards/keychron/q1v2/ansi_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8F, 0xCB, 0x7F, 0x95, 0x56, 0x6C, 0xB7, 0xF2}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
