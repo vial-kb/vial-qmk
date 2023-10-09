ccc2218cd86b1ce31422315cba2ebbc70161b167
adophoxia <100170946+adophoxia@users.noreply.github.com>
2023-10-08 19:00:32 -0700

    Port Keychron Q1v2 to Vial (#568)
    * Add Q1v2
    
    * Add `vial` keymaps for Q1v2

---
+++ keyboards/keychron/q1v2/iso/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8F, 0xAB, 0xE9, 0x65, 0xDA, 0x67, 0x10, 0xAF}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
---
+++ keyboards/keychron/q1v2/jis_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9C, 0x3C, 0xEB, 0x11, 0xA3, 0x3C, 0xB2, 0x38}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
