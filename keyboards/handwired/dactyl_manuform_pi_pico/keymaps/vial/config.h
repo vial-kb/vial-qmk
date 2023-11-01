1a5a47b77ee6541882ba73e1f79aadb2a05b931f
GustawXYZ <gustaw@gustaw.xyz>
2023-11-01 21:21:35 +0100

    add dactyl_manuform_pi_pico vial support

---
+++ keyboards/handwired/dactyl_manuform_pi_pico/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x46, 0x7C, 0x9A, 0xFE, 0xB6, 0x9A, 0xFD, 0x90}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 9
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
