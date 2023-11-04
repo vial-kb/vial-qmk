8d59cb20a42d6741afa462b4ba2bd1799e323cee
Artur Smirnov <reg@artursmirnov.com>
2023-11-04 21:01:24 +0200

    Add support for Aurora Corne by splitkb

---
+++ keyboards/splitkb/aurora/corne/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8A, 0xC7, 0x8B, 0xF1, 0xF2, 0xC7, 0xA8, 0x19}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 6 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
