144e43d900f31a80f06a01ab1e06a047f37594bf
LinuxSBC <benedict.p.simmons@gmail.com>
2023-11-06 22:19:32 -0600

    Add Vial support to Keebio Sinc keyboard (#582)
    * Add Vial support to Keebio Sinc keyboard
    
    * Move Vial keymap to rev4 specifically
    
    * Remove unnecessary layer

---
+++ keyboards/keebio/sinc/rev4/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x58, 0xFD, 0x5B, 0xA6, 0xBB, 0x81, 0x1A, 0x7B}
+
+// Press Escape and Enter to unlock security-sensitive settings 
+#define VIAL_UNLOCK_COMBO_ROWS { 5, 8 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 7 }
+
