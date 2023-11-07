a2255daa3eaf2b072621a55b248ad068ded352b8
Iva Marika Dervisevic <ivan@deepinsight.io>
2023-11-07 16:24:53 +0100

    Add splitkb Aurora Lily58 port

---
+++ keyboards/splitkb/aurora/lily58/rev1/keymaps/vial/rules.mk
@@ -0,0 +1,17 @@
+ENCODER_MAP_ENABLE = yes
+
+# Vial setup
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# Saving space for the default build, remove all/any of these when using a controller with a RP2040 controller
+OLED_ENABLE = no
+MOUSEKEY_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+
+## build targets
+# Liatris
+# CONVERT_TO = liatris
+# promicro
+# BOOTLOADER = caterina
