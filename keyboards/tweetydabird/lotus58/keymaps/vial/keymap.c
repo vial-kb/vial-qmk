bc9e839a1047ffd4ca9ef2045725c136c7be9757
Markus Knutsson <markus.knutsson@tweety.se>
2023-11-08 07:57:02 +0100

    Update keyboards/tweetydabird/lotus58/keymaps/vial/keymap.c
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/tweetydabird/lotus58/keymaps/vial/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/vial/keymap.c
@@ -106,8 +106,6 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         print_status_narrow();
-    } else {
-        print_status_narrow();
     }
 	return false;
 }
