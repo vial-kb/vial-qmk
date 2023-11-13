fdc1a117b2268525bc907c5e0d7466bf1abd034f
supjj <128263666+supjj@users.noreply.github.com>
2023-11-12 23:48:54 -0800

    Apply suggestions from code review
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/vial/config.h
+++ keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/vial/config.h
@@ -80,32 +80,3 @@
 #undef ENABLE_RGB_MATRIX_MULTISPLASH
 #undef ENABLE_RGB_MATRIX_SOLID_SPLASH
 #undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-
-
-// Note: The RGB LED driver configuration is not inside #ifdef blocks, so that
-// the driver code would still compile even without RGBLIGHT_ENABLE and
-// RGB_MATRIX_ENABLE (usually this is not required, but with the custom
-// IS31FL3733+WS2812 driver setup used by this board the LED controller drivers
-// are compiled unconditionally).
-
-// Configure the IS31FL3733 driver for per-key RGB LEDs
-#define DRIVER_COUNT 1
-#define DRIVER_ADDR_1 0b1010000
-#define DRIVER_1_LED_TOTAL 61
-
-#define ISSI_LED_TOTAL DRIVER_1_LED_TOTAL
-
-// Underglow LEDs are WS2812, but someone might want to use RGBLIGHT for them;
-// don't use those LEDs in RGB Matrix in that case.
-#ifdef RGBLIGHT_ENABLE
-#    define WS2812_LED_TOTAL 0
-#else
-#    define WS2812_LED_TOTAL 6
-#endif
-
-#define RGB_MATRIX_LED_COUNT (ISSI_LED_TOTAL + WS2812_LED_TOTAL)
-
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_KEYPRESSES
-#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#endif
