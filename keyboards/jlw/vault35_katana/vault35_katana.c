7c91f7ffa84b13de88bf94fd29595849f3f7e949
avshockeyw <avshockeyw@users.noreply.github.com>
2023-11-25 15:46:47 -0500

    change rgb.c to vault35_katana.c

--- keyboards/jlw/vault35_katana/vault35_katana.c
+++ keyboards/jlw/vault35_katana/vault35_katana.c
@@ -1,4 +1,10 @@
-#include "quantum.h"
+#ifdef RGB_MATRIX_ENABLE
+// LED index
+// 9    10    2    3
+//         1
+//         0
+//   8 7   6   5 4
+
 led_config_t g_led_config = {
     // Key Matrix to LED Index
     {
@@ -26,3 +32,7 @@ led_config_t g_led_config = {
     //  https://docs.qmk.fm/#/feature_rgb_matrix?id=flags
     { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 }
 };
+
+
+#endif
+
