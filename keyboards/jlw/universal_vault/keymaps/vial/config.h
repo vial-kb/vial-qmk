f3716ebbb3c3dc7996153065e0b3ea209d099641
avshockeyw <avshockeyw@users.noreply.github.com>
2023-11-25 21:22:17 -0500

    initial commit
    still need to update RGB and vial.json

---
+++ keyboards/jlw/universal_vault/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE6, 0x15, 0x34, 0x14, 0x86, 0xCC, 0x80, 0x2D}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
+
+#define VIAL_COMBO_ENTRIES 69
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define TAPPING_TERM 250
+
