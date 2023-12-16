7bfa0793c7d9376f72a17105b71811be9aef1952
Вячеслав Истомин <visla.vvi@gmail.com>
2023-12-16 19:10:49 +0300

    Add keyboard: handwired/dactyl_manuform/5x6 (#618)
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8A, 0x86, 0x65, 0x9D, 0x13, 0xE1, 0xCB, 0x81}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
