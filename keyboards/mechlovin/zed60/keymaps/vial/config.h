d5001d8249c9d57352858420526b22ce452596f8
austin4idps <austin@4idps.com>
2023-12-26 13:51:44 +0800

    Port Zed60 to VIAL

---
+++ keyboards/mechlovin/zed60/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Unique keyboard id
+#define VIAL_KEYBOARD_UID {0x86, 0x78, 0xE7, 0x20, 0xAD, 0x90, 0xBB, 0x8E}
+
+// Unlock with Escape+Enter
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
