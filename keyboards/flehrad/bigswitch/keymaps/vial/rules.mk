ac3f6d5eee86b4bb3f7edbbf1ae4890df4cfad94
Ed Griebel <edgriebel@users.noreply.github.com>
2024-01-28 20:36:21 -0500

    flehrad/bigswitch: adding Vial port, expand flash documentation (#649)
    * blehrad/bigswitch: adding Vial port, expand flash docu
    
    * flehrad/bigswitch: default to SECURE mode
    
    * typo
    
    * Apply suggestions from code review
    
    single key as an unlock combo
    
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>
    
    * remove redundant setup info
    
    ---------
    
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

---
+++ keyboards/flehrad/bigswitch/keymaps/vial/rules.mk
@@ -0,0 +1,11 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# These options reduce firmware size to something flashable
+# tap-dance, combos, and key overrides don't make sense with a single button
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+
+# reduces firmware size from 95% to 80%
+LTO_ENABLE = yes
