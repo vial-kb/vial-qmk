d39c1e28ca8cc919ea70e75d16313080f5d5328a
hcker2000 <hcker2000@gmail.com>
2024-02-05 00:56:17 -0500

    Added vial config for Avalanche keyboard (#659)
    * Added vial config
    
    * Added CONVERT_TO
    
    ---------
    
    Co-authored-by: Mike Ashcraft <mashcraft@mach2.org>

---
+++ keyboards/avalanche/v4/keymaps/vial/rules.mk
@@ -0,0 +1,21 @@
+# Build Options
+#   change yes to no to disable
+#
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+ENCODER_ENABLE = yes
+OLED_ENABLE = yes
+
+SPLIT_KEYBOARD = yes
+ENCODER_MAP_ENABLE = yes
+CONVERT_TO = rp2040_ce
