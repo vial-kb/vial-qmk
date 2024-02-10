6dda01b973a865ffbead19123b482d8544fb7b3e
josh-l-wang <22173775+josh-l-wang@users.noreply.github.com>
2024-02-10 01:46:53 -0500

    Add Vault 35 PCB variants: HHKB universal, WKL ortho, WKL universal (#626)
    * initial commit
    
    still need to update RGB and vial.json
    
    * completed vial firmware
    
    used for prototype testing
    
    * change name of board for consistency
    
    * update rgb driver to pwm
    
    * Merge branch 'vault35_wkl_universal' into universal_vault
    
    * update hal/mcuconf files
    
    * fixed compile issues
    
    fixed PWM driver issues and defines. all three included boards compile default keymap without issue. hhkb universal board also compiles vial without issue
    
    * updated vial.jsons
    
    * Update vial UID
    
    * Update vial UID
    
    * update vial UID
    
    * updated matrix
    
    * Update info.json
    
    Updated pins
    
    * Update vial.json
    
    * Update vial.json
    
    * Update vial.json
    
    * update LED drivers
    
    * updated encoder map
    
    * increase eeprom size
    
    ---------
    
    Co-authored-by: avshockeyw <avshockeyw@users.noreply.github.com>

---
+++ keyboards/jlw/vault35_universal/mcuconf.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include_next <mcuconf.h>
+
+//#undef STM32_PWM_USE_TIM15
+//#define STM32_PWM_USE_TIM15 TRUE
+
+//#define WS2812_PWM_CHANNEL 1
---
+++ keyboards/jlw/vault35_wkl_ortho/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC8, 0x15, 0x7F, 0x72, 0x41, 0xAB, 0x8C, 0x1F}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
+
+#define VIAL_COMBO_ENTRIES 69
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define TAPPING_TERM 250
+
