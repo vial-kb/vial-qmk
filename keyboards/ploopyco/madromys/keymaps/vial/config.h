7bdd6f313ab65064481cab81595f50bfb5c502fa
Danny Su <contact@dannysu.com>
2024-02-10 16:16:37 -0800

    Make Ploopy Adept work with Vial

---
+++ keyboards/ploopyco/madromys/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3D, 0x23, 0x68, 0xC4, 0x1F, 0x4E, 0x55, 0x8C}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 3, 5 }
