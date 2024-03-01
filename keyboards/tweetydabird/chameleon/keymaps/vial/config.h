a9f60c449e6cf6ae02794bbab4ff977abfee555c
TweetyDaBird <markus.knutsson@tweety.se>
2024-03-01 14:49:56 +0100

    Initial Chameleon

---
+++ keyboards/tweetydabird/chameleon/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x40, 0x13, 0xA0, 0xCD, 0x14, 0x2B, 0xB5, 0xB0}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
