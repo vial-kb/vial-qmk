17bbd9e3a954033a84d295505f893b1940150430
Harshit Goel <harshitgoel96@yahoo.com>
2024-03-02 15:05:51 +0530

    add support for extra thumb key

--- keyboards/pteron36/keymaps/vial/config.h
+++ keyboards/pteron36/keymaps/vial/config.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xD6, 0x68, 0xF2, 0x55, 0x9B, 0x48, 0x07, 0x6E}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
