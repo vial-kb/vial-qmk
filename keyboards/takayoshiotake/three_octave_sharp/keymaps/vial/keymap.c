ce553092b7842e614e40e926d10f6051100096ad
OTAKE Takayoshi <takayoshiotake@users.noreply.github.com>
2024-03-05 22:24:32 +0900

    Update keymap for takayoshiotake/three_octave_sharp

--- keyboards/takayoshiotake/three_octave_sharp/keymaps/vial/keymap.c
+++ keyboards/takayoshiotake/three_octave_sharp/keymaps/vial/keymap.c
@@ -7,7 +7,7 @@
 #define KC_JKAN KC_JAPANESE_KANA
 
 #define LT_L1ES LT(1, KC_ESC)
-#define MT_LCJE MT(MOD_LGUI, KC_JEIS)
+#define MT_LGJE MT(MOD_LGUI, KC_JEIS)
 #define MT_RAJK MT(MOD_RALT, KC_JKAN)
 
 #define KC_MUTE KC_KB_MUTE
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
 		LT_L1ES, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    MO(2),   MO(3),
-		KC_LCTL, MT_LCJE, KC_SPC,  MT_RAJK, KC_ROPT
+		KC_LCTL, MT_LGJE, KC_SPC,  MT_RAJK, KC_RCTL
 	),
 
 	[1] = LAYOUT(
@@ -30,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 
 	[2] = LAYOUT(
-		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-		KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_DOWN, _______,
-		_______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_RSFT,
+		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______, _______, _______, _______,
+		KC_CAPS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_UP,   KC_DOWN, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, XXXXXXX, KC_RSFT,
 		_______, _______, _______, _______, _______
 	),
 
