db038b0060d40a7180ca6c1e145b3fcf0e3391be
nlnjnj <nlnjnj@gmail.com>
2024-03-11 16:35:18 +0800

    remove QMKBEST codes
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/j80/keymaps/vial/keymap.c
+++ keyboards/j80/keymaps/vial/keymap.c
@@ -15,11 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    QMKBEST = SAFE_RANGE
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_tkl_ansi_split_rshift(
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_PSCR, KC_SCRL, KC_PAUS,
