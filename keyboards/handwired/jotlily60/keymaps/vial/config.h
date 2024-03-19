ab1865bdddb65ccfb3f952fbabdb621925eba8e7
jotix <jujodeve@gmail.com>
2024-03-19 09:42:13 -0300

    add jotlily60 keyboard

---
+++ keyboards/handwired/jotlily60/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+//jotlily60 config.h
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x60, 0x69, 0x48, 0xC6, 0xC5, 0xED, 0x66, 0xA4}
+
+// to unlock the keyboard
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+
+// number of layers
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
