3fe827beabdeaa10cb37261774a21f0142bf6a3a
jotix <jujodeve@gmail.com>
2024-03-19 15:22:07 -0300

    vial.json fixed

--- keyboards/handwired/jotlily60/keymaps/vial/config.h
+++ keyboards/handwired/jotlily60/keymaps/vial/config.h
@@ -5,8 +5,8 @@
 #define VIAL_KEYBOARD_UID {0x60, 0x69, 0x48, 0xC6, 0xC5, 0xED, 0x66, 0xA4}
 
 // to unlock the keyboard
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 8 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
 
 // number of layers
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
