194def1878d9b0ed06e61b892bcd2dfdbcf1a2fd
stanrc85 <stanrc@gmail.com>
2024-04-12 20:10:57 -0400

    gondo updates

--- keyboards/smithrune/ironergo/config.h
+++ keyboards/smithrune/ironergo/config.h
@@ -20,11 +20,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BACKLIGHT_PWM_DRIVER    PWMD3
 #define BACKLIGHT_PWM_CHANNEL   4
 #define BACKLIGHT_PAL_MODE      2
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-#define WEAR_LEVELING_LOGICAL_SIZE 2048
-#define WEAR_LEVELING_BACKING_SIZE 4096
