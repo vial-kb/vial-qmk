a2bcb371468c4378bd2aa974663b185a15311562
Arkaprobho Das <89677542+arko9699@users.noreply.github.com>
2024-08-12 21:22:57 +0530

    Added vial support for For_Science

---
+++ keyboards/for_science/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x89, 0xFB, 0xBB, 0x3C, 0xA4, 0x75, 0x46, 0x11}
+#define VIAL_UNLOCK_COMBO_ROWS { 0,0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0,1 }
