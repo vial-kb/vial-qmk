4ab256cab392c6e81a10b995476e392cf0a7694e
0rax <jp@roemer.im>
2024-10-30 03:22:17 +0100

    Remove rp2040_ce explicit configuration

--- keyboards/minimacro5/rp2040_ce/config.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 JP Roemer (@0rax)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
--- keyboards/minimacro5/rules.mk
+++
@@ -1,1 +0,0 @@
-DEFAULT_FOLDER = minimacro5/promicro
