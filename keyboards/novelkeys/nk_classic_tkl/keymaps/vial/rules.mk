787395302b3417d5e9cd31de0c753022ecb47317
Damien Erambert <damien@erambert.me>
2025-01-25 17:58:27 -0800

    Update rules.mk

--- keyboards/novelkeys/nk_classic_tkl/keymaps/vial/rules.mk
+++ keyboards/novelkeys/nk_classic_tkl/keymaps/vial/rules.mk
@@ -5,6 +5,13 @@ VIA_ENABLE 			= yes
 # Enable VIAL
 VIAL_ENABLE 		= yes
 LTO_ENABLE 			= yes
-
 # Enable RGB
 VIALRGB_ENABLE = yes
+# Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes
+# Mouse keys
+MOUSEKEY_ENABLE = yes
+# Audio control and System control
+EXTRAKEY_ENABLE = yes
+# Enable N-Key Rollover
+NKRO_ENABLE = yes
