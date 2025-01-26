6a49bbe980403009e5369803854e6c6dd9a2ad69
Damien Erambert <damien@erambert.me>
2025-01-25 17:21:53 -0800

    basic vial keymap for nk_classic_tkl

---
+++ keyboards/novelkeys/nk_classic_tkl/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x14, 0xE0, 0xDA, 0x55, 0xA2, 0xB4, 0xD4, 0xF7}
+// Escape + Enter to unlock
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
---
+++ keyboards/novelkeys/nk_classic_tkl/keymaps/vial/rules.mk
@@ -0,0 +1,7 @@
+# Wildcard to allow APM32 MCU (taken from source rules.mk)
+DFU_SUFFIX_ARGS 	= -v FFFF -p FFFF
+# Enable VIA
+VIA_ENABLE 			= yes
+# Enable VIAL
+VIAL_ENABLE 		= yes
+LTO_ENABLE 			= yes
