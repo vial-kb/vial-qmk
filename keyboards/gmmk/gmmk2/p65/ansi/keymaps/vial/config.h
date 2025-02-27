e50abe1fcd17f51ab98b0788592476d8da2826d6
Andrew Litt <al177@users.noreply.github.com>
2025-02-27 17:32:45 -0600

    Add VIAL for GMMK2 65-key ANSI

---
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/vial/config.h
@@ -0,0 +1,26 @@
+/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x88, 0xB0, 0xCD, 0x70, 0x72, 0x1E, 0x47, 0x8E}
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 8 }
+#define VIAL_UNLOCK_COMBO_COLS { 3, 4 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
