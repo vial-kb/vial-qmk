19e9addc9fd9c0f60c89b9618035fe6c736c9d24
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-04-01 11:49:33 -0400

    add vial for kb16

---
+++ keyboards/doio/kb16/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x65, 0x9A, 0x6E, 0x3C, 0x47, 0x63, 0xB9, 0x9B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
