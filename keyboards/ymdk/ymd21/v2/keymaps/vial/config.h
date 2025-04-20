9df4deaf0a66ca39f83f84e1fe47d03f54ebb30a
sergey radionov <rsatom@gmail.com>
2025-04-20 17:45:19 +0700

    Vial support for YMDK YMD21 V2

---
+++ keyboards/ymdk/ymd21/v2/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFF, 0xF4, 0xB0, 0xEC, 0xBE, 0xE1, 0x35, 0x5C}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
---
+++ keyboards/ymdk/ymd21/v2/keymaps/vial/rules.mk
@@ -0,0 +1,7 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+QMK_SETTINGS = no
+KEY_OVERRIDE_ENABLE = no
+LTO_ENABLE = yes
+
+
