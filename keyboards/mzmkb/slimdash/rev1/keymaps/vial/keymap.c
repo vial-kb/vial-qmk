2432414670d9648fb23122837446c3a603b9baf5
mizma <omoikane@path-works.net>
2025-05-04 19:50:30 +0900

    fix keymap for RGB backlight control

--- keyboards/mzmkb/slimdash/rev1/keymaps/vial/keymap.c
+++ keyboards/mzmkb/slimdash/rev1/keymaps/vial/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,            KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,            KC_INS,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,            KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        RM_VALD, RM_VALU, GAMEM,   KC_LGUI, LOWER,   KC_SPC,  KC_LSFT,            KC_RCTL, KC_ENT,  RAISE,   KC_HOME, KC_END,  KC_PGUP, KC_PGDN
+        RGB_VAD, RGB_VAI, GAMEM,   KC_LGUI, LOWER,   KC_SPC,  KC_LSFT,            KC_RCTL, KC_ENT,  RAISE,   KC_HOME, KC_END,  KC_PGUP, KC_PGDN
     ),
 
     /* Game Mode Toggle: sample toggle layer.  turn off GUI key and add arrows
@@ -94,9 +94,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [_LOWER] = LAYOUT(
         KC_GRV,  _______, _______, _______, _______, KC_MUTE, KC_VOLD,            KC_VOLU, _______, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, _______,
-        _______, _______, _______, _______, RM_TOGG, _______, KC_BRID,            KC_BRIU, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
-        KC_CAPS, _______, _______, RM_HUED, RM_HUEU, _______, RM_PREV,            RM_NEXT, _______, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, _______,
-        _______, _______, _______, RM_SPDD, RM_SPDU, _______, _______,            _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, _______,
+        _______, _______, _______, _______, RGB_TOG, _______, KC_BRID,            KC_BRIU, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
+        KC_CAPS, _______, _______, RGB_HUD, RGB_HUI, _______, RGB_RMOD,           RGB_MOD, _______, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, _______,
+        _______, _______, _______, RGB_SPD, RGB_SPI, _______, _______,            _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, _______,
         _______, _______, _______, _______, _______, _______, _______,            _______, KC_PENT, _______, KC_P0,   KC_PDOT, _______, _______
     ),
 
