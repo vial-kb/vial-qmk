93247405bbb4b2bf06edbfebbac5f38640f2db73
igor poughtach <ingvardm@gmail.com>
2025-05-10 11:54:09 +0300

    ported keebio nyquist

---
+++ keyboards/keebio/nyquist/keymaps/vial/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+VIALRGB_ENABLE      = yes
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = yes
+
+ENCODER_MAP_ENABLE  = yes
+MOUSEKEY_ENABLE     = no
+COMBO_ENABLE        = yes
+MUSIC_ENABLE        = no
+
+QMK_SETTINGS        = yes
