064330d60b6d6930169905639261b9ffc6333570
igor poughtach <ingvardm@gmail.com>
2025-05-12 19:58:51 +0300

    initial port

---
+++ keyboards/magic_force/mf17/keymaps/vial/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+ENCODER_MAP_ENABLE  = no
+MOUSEKEY_ENABLE     = no
+MUSIC_ENABLE        = no
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = yes
+VIALRGB_ENABLE      = yes
+COMBO_ENABLE        = yes
+
+QMK_SETTINGS        = yes
