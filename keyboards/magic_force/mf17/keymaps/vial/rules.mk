d29589d11b9a5e68525d2fe53622e6a4678b0861
igor poughtach <ingvardm@gmail.com>
2025-05-12 21:32:19 +0300

    expand features

--- keyboards/magic_force/mf17/keymaps/vial/rules.mk
+++ keyboards/magic_force/mf17/keymaps/vial/rules.mk
@@ -5,7 +5,9 @@ LTO_ENABLE          = yes
 ENCODER_MAP_ENABLE  = no
 MOUSEKEY_ENABLE     = no
 MUSIC_ENABLE        = no
-
+CONSOLE_ENABLE      = no
+COMMAND_ENABLE      = no
+KEY_OVERRIDE_ENABLE = no
 RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = yes
 VIALRGB_ENABLE      = yes
