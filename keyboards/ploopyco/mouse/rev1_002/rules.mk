a3a991cd6674f90d92e4b8739ebe0fa9258d606b
shadow-absorber <sam.tunder@gmail.com>
2025-05-31 18:39:05 +0200

    change folder structure and add new keyboard id fixes vial feature set for old and new ploopy mouse. this is achieved by moving the keymaps folder into the respective folders. also change the keyboard id for the new revision.

--- keyboards/ploopyco/mouse/rev1_002/rules.mk
+++ keyboards/ploopyco/mouse/rev1_002/rules.mk
@@ -1,9 +1,3 @@
 # Processor frequency
 F_CPU = 8000000
 
-TAP_DANCE_ENABLE = no
-KEY_OVERRIDE_ENABLE = no
-
-
-LTO_ENABLE = yes
-
---
+++ keyboards/ploopyco/mouse/rev1_003/keymaps/vial/config.h
@@ -0,0 +1,27 @@
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ * Copyright 2019 Sunjun Kim
+ * Copyright 2020 Ploopy Corporation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define PLOOPY_DPI_OPTIONS { 800, 1200, 1600, 2400 }
+#define PLOOPY_DPI_DEFAULT 0
+
+#define VIAL_KEYBOARD_UID {0x78, 0x87, 0x0E, 0x2A, 0x8C, 0xB2, 0xB4, 0x7A}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {4, 5}
