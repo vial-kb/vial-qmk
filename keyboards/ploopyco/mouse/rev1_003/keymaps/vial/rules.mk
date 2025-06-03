a82854afa74d4530e9cc17b7969c00bbcc2652cd
shadow-absorber <sam.tunder@gmail.com>
2025-06-03 18:46:09 +0200

    remove commented out lines

--- keyboards/ploopyco/mouse/rev1_003/keymaps/vial/rules.mk
+++ keyboards/ploopyco/mouse/rev1_003/keymaps/vial/rules.mk
@@ -6,11 +6,3 @@ POINTING_DEVICE_DRIVER = pmw3360
 # add vial config
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
-
-# TAP_DANCE_ENABLE = no
-# KEY_OVERRIDE_ENABLE = no
-
-
-# LTO_ENABLE = yes
-
-# VIAL_INSECURE = yes
