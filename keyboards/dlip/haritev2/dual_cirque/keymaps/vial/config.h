8c8f5255ba0486d8755506991f749204924770e4
Dane Lipscombe <danelipscombe@gmail.com>
2025-06-16 09:18:33 +1000

    Add Harite v2

---
+++ keyboards/dlip/haritev2/dual_cirque/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC5, 0xE2, 0xC7, 0xBD, 0x68, 0xC9, 0x59, 0xB7}
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
