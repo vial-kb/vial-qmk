18787226b347d5d382103e2035dcf6627011171d
Dane Lipscombe <danelipscombe@gmail.com>
2025-06-16 10:30:10 +1000

    Fix keycode

--- keyboards/dlip/haritev2/dual_cirque/keymaps/vial/keymap.c
+++ keyboards/dlip/haritev2/dual_cirque/keymaps/vial/keymap.c
@@ -5,7 +5,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_5x6(
               KC_U,              KC_U,              KC_U,              KC_U,                    KC_U,              KC_U,              KC_U,              KC_U,
-        KC_L, QK_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,        KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, QK_C, KC_R,
+        KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,        KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,  KC_L, KC_C, KC_R,
               KC_D,              KC_D,              KC_D,              KC_D,                    KC_D,              KC_D,              KC_D,              KC_D,
 
                                                                        KC_U,                    KC_U,
