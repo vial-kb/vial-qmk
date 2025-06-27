34024b54b4344ceeda98969d5a83923c590e81bf
sunsetsonwheels <hey@sunsetsonwheels.me>
2025-06-28 01:16:43 +1000

    Add zed60 vial support

---
+++ keyboards/mechlovin/zed60/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2B, 0x0A, 0xDC, 0xF4, 0xC9, 0x54, 0xDE, 0xDF}
+
+// Esc + Enter
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
