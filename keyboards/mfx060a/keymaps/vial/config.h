b3f2b30f0069b83b542e6f2ea431ea014b841b49
ms06j <ms06j.ggg@gmail.com>
2025-07-10 23:56:33 +0900

    Add a new keyboard for mfx060a.

---
+++ keyboards/mfx060a/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8A, 0xDB, 0x0D, 0x9F, 0x7E, 0xB6, 0x7F, 0x1A}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
