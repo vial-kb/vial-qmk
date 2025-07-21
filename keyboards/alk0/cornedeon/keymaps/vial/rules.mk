31b411c83fbed7f62811558b3401b5af762b40cc
ALK0 <alko-kbd@alk0.ru>
2025-07-21 11:59:47 +0500

    Update keyboards/alk0/cornedeon/keymaps/vial/rules.mk
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/alk0/cornedeon/keymaps/vial/rules.mk
+++ keyboards/alk0/cornedeon/keymaps/vial/rules.mk
@@ -2,8 +2,7 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = no
 
-RGBLIGHT_ENABLE     = no
-RGB_MATRIX_ENABLE   = yes # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+RGB_MATRIX_ENABLE   = yes
 VIALRGB_ENABLE      = yes
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = no
