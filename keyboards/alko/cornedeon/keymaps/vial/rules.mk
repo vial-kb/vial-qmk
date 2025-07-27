d447a713ab4ff0df1e6809482452647fe879657f
alko <alko@alk0.ru>
2025-07-27 19:38:03 +0500

    Fix vial rules.mk for rev1_5 compatibility

--- keyboards/alko/cornedeon/keymaps/vial/rules.mk
+++ keyboards/alko/cornedeon/keymaps/vial/rules.mk
@@ -1,15 +1,15 @@
 VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
-LTO_ENABLE          = no
+LTO_ENABLE          = yes
 
 RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = yes # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 VIALRGB_ENABLE      = yes
-MOUSEKEY_ENABLE     = yes
+MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = no
 OLED_DRIVER         = SSD1306
-EXTRAKEY_ENABLE     = yes
-COMBO_ENABLE        = yes
+EXTRAKEY_ENABLE     = no
+COMBO_ENABLE        = no
 
 QMK_SETTINGS        = no
 
