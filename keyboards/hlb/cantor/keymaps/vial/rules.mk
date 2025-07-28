4a7f27e0415655d5b89662a9d17dedee857c577c
H3lli0n <thulium.hqfr@gmail.com>
2025-07-28 23:04:09 +0200

    fix: bootloader definition

--- keyboards/hlb/cantor/keymaps/vial/rules.mk
+++ keyboards/hlb/cantor/keymaps/vial/rules.mk
@@ -2,7 +2,6 @@ VIA_ENABLE  = yes    # Enable VIA
 VIAL_ENABLE = yes    # Enable VIAL
 
 #We need to specify bootloader and MCU for F411 8Mhz version of BlackPillS
-BOOTLOADER = stm32-dfu
 MCU = STM32F411
 
 #We are in split keyboard mode
