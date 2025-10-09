12a0c8192509042e21c1f3508d1ef475dc90e01d
Erwin Jansen <pokowaka@users.noreply.github.com>
2025-10-08 21:37:15 -0700

    feat(atreus62): Vial support
    This adds support for vial to the atreus62 board.
    It includes a proper layout displaying the keyboard.
    
    Test: make atreus62:vial
    Checking file size of atreus62_vial.hex                                                             [WARNINGS]
    
    * The firmware size is approaching the maximum - 27890/28672 (97%, 782 bytes free)

---
+++ keyboards/atreus62/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA4, 0x1E, 0xBD, 0x61, 0xE5, 0xD7, 0x51, 0xE4}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
---
+++ keyboards/atreus62/keymaps/vial/rules.mk
@@ -0,0 +1,11 @@
+VIA_ENABLE 					= yes
+VIAL_ENABLE 				= yes
+LTO_ENABLE 					= yes
+QMK_SETTINGS 				= no
+# Disable mousekey below if you wish to have a few bytes more available.
+# MOUSEKEY_ENABLE 			= no 
+
+# Logging will push the firmware size over the edge!
+CONSOLE_ENABLE			 	= no
+DEBUG_MATRIX_SCAN_RATE   	= no
+DEBUG_MATRIX				= no
