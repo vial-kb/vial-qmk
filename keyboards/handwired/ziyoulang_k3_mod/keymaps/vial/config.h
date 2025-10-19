f9ccb6334b3679d12b273a32b3a466de204507f6
Coom <kazunori.takahashi.g@gmail.com>
2025-10-20 00:07:26 +0900

    Add support handwired/ziyoulang_k3_mod

---
+++ keyboards/handwired/ziyoulang_k3_mod/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x01, 0x0B, 0x6B, 0xD3, 0x32, 0xC7, 0xB0, 0x93}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 20 }
