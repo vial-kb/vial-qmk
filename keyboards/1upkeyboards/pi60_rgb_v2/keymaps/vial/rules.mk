97383d528c8f131b27b4ceb4c3fc69e25273f074
Joan Delilah Pepin <delilah@Mac.lan>
2025-11-16 17:26:36 -0800

    Revert "Enable eager debouncing for 1upkeyboards/pi60_rgb_v2"
    This reverts commit 07f468ff91138c9795cf74ef59a224a7b40c2f97.

--- keyboards/1upkeyboards/pi60_rgb_v2/keymaps/vial/rules.mk
+++ keyboards/1upkeyboards/pi60_rgb_v2/keymaps/vial/rules.mk
@@ -2,9 +2,3 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 LTO_ENABLE = yes
 VIALRGB_ENABLE = yes
-
-# Use eager debouncing for faster response time
-# The RP2040 has plenty of processing power to handle eager debouncing,
-# which provides better responsiveness for gaming by immediately registering
-# key presses while only debouncing the key release.
-DEBOUNCE_TYPE = sym_eager_pk
