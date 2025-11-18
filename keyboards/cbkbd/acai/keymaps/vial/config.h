b578e1ae4a913b66aa216621fdfd12df71a558f9
andrew morton <drewish@katherinehouse.com>
2025-11-18 09:03:00 -0700

    Add Vial support for Coffee Break Keyboards Acai

---
+++ keyboards/cbkbd/acai/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x65, 0xC8, 0x0C, 0x2C, 0x53, 0x74, 0x62, 0xE2}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0}
+#define VIAL_UNLOCK_COMBO_COLS {0}
