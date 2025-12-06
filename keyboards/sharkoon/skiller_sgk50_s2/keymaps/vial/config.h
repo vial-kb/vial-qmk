44d9546c065b11f319c7acbeb1ba74d142e0a901
dkleber89 <dkleber89@gmail.com>
2025-12-06 18:43:42 +0100

    Fix

--- keyboards/sharkoon/skiller_sgk50_s2/keymaps/vial/config.h
+++ keyboards/sharkoon/skiller_sgk50_s2/keymaps/vial/config.h
@@ -4,6 +4,6 @@
 
 #define VIAL_KEYBOARD_UID {0xEE, 0x52, 0xE5, 0xC4, 0x39, 0x8D, 0x63, 0xF7}
 
-// Unlock combo esc and enter
+// Unlock combo ctrl and enter
 #define VIAL_UNLOCK_COMBO_ROWS {5, 3}
 #define VIAL_UNLOCK_COMBO_COLS {0, 13}
