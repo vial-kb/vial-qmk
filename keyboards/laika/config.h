90261c93757fb5dcf6646d63a62b8459b44fa1b2
kapee1 <volodya1907@gmail.com>
2025-12-15 10:49:50 +0600

    initial commit for Laika keyboard

---
+++ keyboards/laika/config.h
@@ -0,0 +1,2 @@
+// Copyright 2025 kapee1 (@kapee1)
+// SPDX-License-Identifier: GPL-2.0-or-later
---
+++ keyboards/laika/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x41, 0x50, 0x1C, 0xEF, 0x87, 0xE1, 0xDA, 0x31}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 0 }
