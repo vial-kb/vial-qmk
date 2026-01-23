9d38888dbfcd8a306cc505ab749adeec20fe9643
Maryam Sheikh <mahid@standingpad.org>
2026-01-23 12:30:04 -0600

    Add support for the Keychron C2 V2 White
    This adds support for the Keychron C2 V2 White. The RGB version is not
    supported due to not owning an RGB C2 V2.
    
    One thing this is missing is backlight controls, since the Keychron C2
    V2 White uses LED Matrix (https://docs.qmk.fm/features/led_matrix).
    While this is a feature in upstream QMK, and seems to be here, I'm not
    sure if Vial explicitly supports LED Matrix in the GUI. LED Matrix is
    not to be confused with RGB Matrix (the former being with single-color
    LEDs and not RGB LEDs).
    
    Signed-off-by: Maryam Sheikh <mahid@standingpad.org>

---
+++ keyboards/keychron/c2_pro_v2/ansi/white/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9B, 0xB2, 0x2D, 0x80, 0x32, 0x37, 0x16, 0x1C}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
