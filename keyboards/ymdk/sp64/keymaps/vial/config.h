a253cd9ce6f91f73ad6eb6c889159f383d6d5b1f
Will Fuller <sinistergfx@yahoo.com>
2026-01-31 11:28:50 -0600

    Add Vial keymap

---
+++ keyboards/ymdk/sp64/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF7, 0x20, 0x5E, 0xE3, 0x8E, 0x8D, 0x0F, 0xC9}
+#define VIAL_UNLOCK_COMBO_ROWS { 5, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
