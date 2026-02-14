af3429c6d795114e46559b06408c511ff4610ebc
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2026-02-15 00:03:13 +1100

    fix(10-03): DIAG-05 restore full-duplex and replace SPLIT_USB_DETECT with MASTER_LEFT
    - Uncomment SERIAL_USART_FULL_DUPLEX (restore from DIAG-02 half-duplex test)
    - Comment out SPLIT_USB_DETECT to eliminate USB detection timing as variable
    - Add MASTER_LEFT to hardcode left half as master
    - Both halves compile cleanly (left: 125,952 bytes, right: 108,544 bytes)

--- keyboards/hidpress/bipedalambi/config.h
+++ keyboards/hidpress/bipedalambi/config.h
@@ -3,8 +3,9 @@
 // Common Configuration for Bipedal Ambi
 // Specific configuration (Pins, Matrix, I2C) is in keymaps/vial_left and keymaps/vial_right
 
-// #define SERIAL_USART_FULL_DUPLEX  // DIAG-02: disabled for half-duplex test
-#define SPLIT_USB_DETECT
+#define SERIAL_USART_FULL_DUPLEX
+// #define SPLIT_USB_DETECT  // DIAG-05: disabled for MASTER_LEFT test
+#define MASTER_LEFT  // DIAG-05: hardcode left as master
 #define SERIAL_PIO_USE_PIO1  // Use PIO1 for serial (PIO0 used by WS2812)
 #define SPLIT_LAYER_STATE_ENABLE
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
