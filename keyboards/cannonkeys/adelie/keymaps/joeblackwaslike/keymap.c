d20f3df2c6947b3c09171cf2ed5a929238947cb1
Joe Black <me@joeblack.nyc>
2026-03-20 13:38:33 -0400

    updated keymap

--- keyboards/cannonkeys/adelie/keymaps/joeblackwaslike/keymap.c
+++ keyboards/cannonkeys/adelie/keymaps/joeblackwaslike/keymap.c
@@ -29,32 +29,68 @@ enum layer_names {
     _FN3
 };
 
+#define SFT_LBRC LSFT_T(KC_LBRC)
+#define SFT_RBRC RSFT(KC_RBRC)
+#define ALL_BSPC ALL_T(KC_BSPC)
+
+#define L2_TAB LT(2,KC_TAB)
+
+#define CTL_A LCTL_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define GUI_D LGUI_T(KC_D)
+#define SFT_F LSFT_T(KC_F)
+#define ALL_G ALL_T(KC_G)
+#define ALL_H ALL_T(KC_H)
+#define SFT_J RSFT_T(KC_J)
+#define GUI_K RGUI_T(KC_K)
+#define ALT_L RALT_T(KC_L)
+#define CTL_SCLN RCTL_T(KC_SCLN)
+
+#define L2_ENT LT(2,KC_ENT)
+
+#define CTL_ESC LCTL_T(KC_ESC)
+#define L1_SPC LT(1,KC_SPC)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_BASE] = LAYOUT_all(
-        KC_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_VOLU,
-        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_VOLD,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_MUTE,
-        KC_LCTL, KC_LALT, KC_LGUI, MO(_FN1), LT(_FN2, KC_SPC), KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    [_BASE] = LAYOUT_all(
+        KC_GRV, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     SFT_LBRC, SFT_RBRC,   ALL_BSPC,   KC_VOLU,
+        L2_TAB, CTL_A,  ALT_S,  GUI_D,  SFT_F,  ALL_G,  ALL_H,  SFT_J,  GUI_K,  ALT_L,  CTL_SCLN, KC_QUOT,  L2_ENT,                 KC_VOLD,
+        SC_LSPO,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  SC_RSPC,              KC_UP,      KC_MUTE,
+        CTL_ESC,KC_LALT,KC_LGUI,        L1_SPC,                 L1_SPC,         KC_RGUI,KC_RALT,  KC_RCTL,  KC_LEFT,    KC_DOWN,    KC_RGHT
     ),
-	[_FN1] = LAYOUT_all(
-        KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_ASTR, KC_EQL, KC_BSLS, KC_BRIU,
-        _______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BRID,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, RESET,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
+
+    [_FN1] = LAYOUT_all(
+        KC_ESC, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,  KC_UNDS, KC_PLUS, KC_PIPE, KC_BRIU,
+        KC_TRNS, KC_1,  KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS, KC_EQL,  KC_BRID,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_BSLS, KC_TRNS, KC_PGUP, KC_CAPS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LGUI(KC_LBRC),    KC_PGDN, LGUI(KC_RBRC)
     ),
-	[_FN2] = LAYOUT_all(
-        _______, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, _______, RGB_TOG,
-        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, RGB_MOD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, RGB_SPI,
-        _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUD, RGB_VAD, RGB_HUI
+
+    [_FN2] = LAYOUT_all(
+        QK_BOOT,KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22,   KC_F23,  KC_F24,  KC_TRNS, RGB_TOG, KC_TRNS,
+        KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F12,   KC_TRNS, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,RGB_VAI,RGB_SPI,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,KC_TRNS,KC_TRNS,RGB_HUD,RGB_VAD,RGB_HUI
     ),
+
     [_FN3] = LAYOUT_all(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS, MO(1), KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
+
+const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_GUI, KC_GRV, KC_ESC);
+
+const uint16_t PROGMEM toggle_layer_1_on[] = {KC_VOLU, KC_VOLD, COMBO_END};
+const uint16_t PROGMEM toggle_layer_1_off[] = {KC_BRIU, KC_BRID, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(toggle_layer_1_on, TG(1)),
+    COMBO(toggle_layer_1_off, TG(1)), // keycodes with modifiers are possible too!
+};
+
 
 const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 3, HSV_RED}
